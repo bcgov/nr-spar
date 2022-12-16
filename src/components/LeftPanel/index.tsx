f89b71bbc2f6ad707961825e2521fac2a7bba987
Ricardo Campos <ricardo.campos@encora.com>
2022-12-16 10:21:39 -0300

    Feat: show current version (#14)
    * feat: show version in the left side panel bottom
    
    * ci: enable to generate empty changelog to stop blocking deployments

--- src/components/LeftPanel/index.tsx
+++ src/components/LeftPanel/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { SideNav, SideNavItems, SideNavLink } from '@carbon/react';
 
 import * as Icons from '@carbon/icons-react';
+import { env } from '../../env';
 
 interface ListItem {
   name: string;
@@ -19,34 +20,39 @@ interface LeftPanelProps {
   listItems: ListItems[];
 }
 
-const LeftPanel = ({ listItems }: LeftPanelProps) => (
-  <SideNav isFixedNav expanded isChildOfHeader aria-label="Side navigation">
-    <SideNavItems>
-      {listItems.map((item: ListItems) => (
-        <div key={item.name}>
-          <SideNavLink key={item.name} title={item.name} disabled>
-            {item.name}
-          </SideNavLink>
-          {item.items.map((subItem: ListItem) => {
-            const IconComponent = Icons[subItem.icon];
-            return (
-              <SideNavLink
-                key={subItem.name}
-                renderIcon={IconComponent || ''}
-                href={!subItem.disabled ? subItem.link : null}
-                isActive={window.location.pathname === subItem.link}
-              >
-                {subItem.name}
-              </SideNavLink>
-            );
-          })}
-        </div>
-      ))}
+const LeftPanel = ({ listItems }: LeftPanelProps) => {
+  const version: string = `Version: ${env.REACT_APP_NRFESAMPLEAPP_VERSION}`;
 
-      <SideNavLink disabled>Support</SideNavLink>
-      <SideNavLink renderIcon={Icons.Help}>Need help?</SideNavLink>
-    </SideNavItems>
-  </SideNav>
-);
+  return (
+    <SideNav isFixedNav expanded isChildOfHeader aria-label="Side navigation">
+      <SideNavItems>
+        {listItems.map((item: ListItems) => (
+          <div key={item.name}>
+            <SideNavLink key={item.name} title={item.name} disabled>
+              {item.name}
+            </SideNavLink>
+            {item.items.map((subItem: ListItem) => {
+              const IconComponent = Icons[subItem.icon];
+              return (
+                <SideNavLink
+                  key={subItem.name}
+                  renderIcon={IconComponent || ''}
+                  href={!subItem.disabled ? subItem.link : null}
+                  isActive={window.location.pathname === subItem.link}
+                >
+                  {subItem.name}
+                </SideNavLink>
+              );
+            })}
+          </div>
+        ))}
+
+        <SideNavLink disabled>Support</SideNavLink>
+        <SideNavLink renderIcon={Icons.Help}>Need help?</SideNavLink>
+        <SideNavLink disabled>{version}</SideNavLink>
+      </SideNavItems>
+    </SideNav>
+  );
+};
 
 export default LeftPanel;
