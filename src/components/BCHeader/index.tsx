c183ce01f9ced95c35e8997ed5b4a6ca5bf6c962
mgaseta <105936322+mgaseta@users.noreply.github.com>
2022-12-30 13:33:43 -0300

    feat: dashboard breakpoints  (#23)
    * feat: added responsiviness to the dashboard page
    
    * fix: adjusting card styles file
    
    * fix: adjusting merge main errors
    
    * fix: correctly merging after recent PRs
    
    * fix: applied markups and corrected the opened overflow menu location
    
    * fix: adjusted comment on header style

--- src/components/BCHeader/index.tsx
+++ src/components/BCHeader/index.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import {
   Button,
+  HeaderContainer,
   Header,
+  SkipToContent,
+  HeaderMenuButton,
   HeaderGlobalBar,
   HeaderGlobalAction,
-  Theme
+  SideNav,
+  SideNavItems,
+  SideNavLink
 } from '@carbon/react';
-import {
-  Search,
-  Notification,
-  UserAvatar
-} from '@carbon/icons-react';
-import { Link, useNavigate } from 'react-router-dom';
+import * as Icons from '@carbon/icons-react';
+
+import { env } from '../../env';
+
+import PanelSectionName from '../PanelSectionName';
+
+import LeftPanelItems from '../../mock-data/LeftPanelItems';
+
 import './styles.scss';
 
+interface ListItem {
+  name: string;
+  icon: string;
+  link: string;
+  disabled: boolean;
+}
+interface ListItems {
+  name: string;
+  items: ListItem[]
+}
+
+const listItems = LeftPanelItems;
+
 const BCHeader = () => {
+  const version: string = `Version: ${env.REACT_APP_NRFESAMPLEAPP_VERSION}`;
+
   const navigate = useNavigate();
 
   const goOut = () => {
@@ -23,31 +46,72 @@ const BCHeader = () => {
   };
 
   return (
-    <Theme theme="g100">
-      <Header aria-label="BC Gov's NR Sample App" data-testid="header">
-        <Link to="/dashboard" className="header-link" data-testid="header-name">
-          SPAR
-          <span className="header-full-name"> Seed Planning and Registry System</span>
-        </Link>
-        <HeaderGlobalBar>
-          <Button
-            onClick={() => goOut()}
-            size="sm"
-          >
-            Logout
-          </Button>
-          <HeaderGlobalAction aria-label="Search" data-testid="header-button__search">
-            <Search size={20} />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="Notifications" data-testid="header-button__notifications">
-            <Notification size={20} />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="User Settings" tooltipAlignment="end" data-testid="header-button__user">
-            <UserAvatar size={20} />
-          </HeaderGlobalAction>
-        </HeaderGlobalBar>
-      </Header>
-    </Theme>
+    <HeaderContainer
+      render={({ isSideNavExpanded, onClickSideNavExpand }: any) => (
+        <Header
+          aria-label="Seed Planning and Registry System"
+          className="spar-header"
+          data-testid="header"
+        >
+          <SkipToContent />
+          <HeaderMenuButton
+            aria-label="Open menu"
+            onClick={onClickSideNavExpand}
+            isActive={isSideNavExpanded}
+          />
+          <Link to="/dashboard" className="header-link" data-testid="header-name">
+            SPAR
+            <span className="header-full-name"> Seed Planning and Registry System</span>
+          </Link>
+          <HeaderGlobalBar>
+            <Button
+              onClick={() => goOut()}
+              size="sm"
+            >
+              Logout
+            </Button>
+            <HeaderGlobalAction aria-label="Search" data-testid="header-button__search">
+              <Icons.Search size={20} />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="Notifications" data-testid="header-button__notifications">
+              <Icons.Notification size={20} />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="User Settings" tooltipAlignment="end" data-testid="header-button__user">
+              <Icons.UserAvatar size={20} />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+          <SideNav isChildOfHeader expanded={isSideNavExpanded} aria-label="Side menu">
+            <SideNavItems>
+              {listItems.map((item: ListItems) => (
+                <div key={item.name}>
+                  <PanelSectionName title={item.name} />
+                  {item.items.map((subItem: ListItem) => {
+                    const IconComponent = Icons[subItem.icon];
+                    return (
+                      <SideNavLink
+                        key={subItem.name}
+                        renderIcon={IconComponent || ''}
+                        onClick={() => {
+                          navigate(`${subItem.link}`);
+                        }}
+                        isActive={window.location.pathname === subItem.link}
+                      >
+                        {subItem.name}
+                      </SideNavLink>
+                    );
+                  })}
+                </div>
+              ))}
+              <div className="support-section">
+                <PanelSectionName title="Support" />
+                <SideNavLink renderIcon={Icons.Help}>Need help?</SideNavLink>
+                <PanelSectionName title={version} />
+              </div>
+            </SideNavItems>
+          </SideNav>
+        </Header>
+      )}
+    />
   );
 };
 
--- src/layout/PublicLayout/index.tsx
+++ src/layout/PublicLayout/index.tsx
@@ -5,19 +5,12 @@ import { Content } from '@carbon/react';
 
 import BCHeader from '../../components/BCHeader';
 
-import LeftPanel from '../../components/LeftPanel';
-
 import './styles.scss';
-
-import LeftPanelItems from '../../mock-data/LeftPanelItems';
-
-const listItems = LeftPanelItems;
 
 const Layout = () => (
   <>
     <BCHeader />
     <div className="mainContainer">
-      <LeftPanel listItems={listItems} />
       <Content className="page-content">
         <Outlet />
       </Content>
