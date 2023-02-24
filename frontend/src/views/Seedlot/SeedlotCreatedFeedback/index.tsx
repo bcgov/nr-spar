447f6c204b2a28b5c60f097d34692a5a968a6cd3
mgaseta <105936322+mgaseta@users.noreply.github.com>
2023-02-24 13:46:00 -0300

    feat: linked a class form to feedback page (#59)

--- frontend/src/views/Seedlot/SeedlotCreatedFeedback/index.tsx
+++ frontend/src/views/Seedlot/SeedlotCreatedFeedback/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   FlexGrid,
   Column,
@@ -8,45 +9,49 @@ import {
 import * as Pictograms from '@carbon/pictograms-react';
 import './styles.scss';
 
-const SeedlotCreatedFeedback = () => (
-  <FlexGrid fullWidth className="seedlot-created-feedback-page">
-    <Row className="scf-row">
-      <Column className="scf-pic-container">
-        <Pictograms.Growth className="scf-pictogram" />
-      </Column>
-      <Column className="scf-info-container">
-        <h1> A class seedlot created! </h1>
-        <h2>
-          Your A class seedlot has been created with success!
-          Now you can access the seedlot’s detail screen,
-          create another one or go back to the seedlot’s main screen.
-        </h2>
-        <Button
-          onClick={() => null}
-          size="lg"
-          className="btn-scf"
-        >
-          Go to seedlot’s detail screen
-        </Button>
-        <Button
-          kind="tertiary"
-          onClick={() => null}
-          size="lg"
-          className="btn-scf"
-        >
-          Create another A class seedlot
-        </Button>
-        <Button
-          kind="tertiary"
-          onClick={() => null}
-          size="lg"
-          className="btn-scf"
-        >
-          Go back to seedlot’s main screen
-        </Button>
-      </Column>
-    </Row>
-  </FlexGrid>
-);
+const SeedlotCreatedFeedback = () => {
+  const navigate = useNavigate();
+
+  return (
+    <FlexGrid fullWidth className="seedlot-created-feedback-page">
+      <Row className="scf-row">
+        <Column className="scf-pic-container">
+          <Pictograms.Growth className="scf-pictogram" />
+        </Column>
+        <Column className="scf-info-container">
+          <h1> A class seedlot created! </h1>
+          <h2>
+            Your A class seedlot has been created with success!
+            Now you can access the seedlot's detail screen,
+            create another one or go back to the seedlot's main screen.
+          </h2>
+          <Button
+            onClick={() => null}
+            size="lg"
+            className="btn-scf"
+          >
+            Go to seedlot's detail screen
+          </Button>
+          <Button
+            kind="tertiary"
+            onClick={() => navigate('/seedlot/register-a-class')}
+            size="lg"
+            className="btn-scf"
+          >
+            Create another A class seedlot
+          </Button>
+          <Button
+            kind="tertiary"
+            onClick={() => navigate('/seedlot')}
+            size="lg"
+            className="btn-scf"
+          >
+            Go back to seedlot's main screen
+          </Button>
+        </Column>
+      </Row>
+    </FlexGrid>
+  );
+};
 
 export default SeedlotCreatedFeedback;
