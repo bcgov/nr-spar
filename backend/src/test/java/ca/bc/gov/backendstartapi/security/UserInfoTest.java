b42374be430b74ef24c4021215360cbecbeee226
Derek Roberts <derek.roberts@gmail.com>
2023-03-07 10:44:32 -0800

    Fix backend structure

---
+++ backend/src/test/java/ca/bc/gov/backendstartapi/security/UserInfoTest.java
@@ -0,0 +1,35 @@
+package ca.bc.gov.backendstartapi.security;
+
+import java.util.HashSet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserInfoTest {
+
+  @Test
+  @DisplayName("createUserInfo")
+  void createUserInfo() {
+    UserInfo userInfo =
+        new UserInfo(
+            "123456789@idir",
+            "Bilbo",
+            "Baggings",
+            "bilbo.baggings@gov.bc.ca",
+            "Baggings, Bilbo LWRS:EX",
+            "BAGGINGS",
+            null,
+            "idir",
+            new HashSet<>());
+
+    Assertions.assertNotNull(userInfo);
+    Assertions.assertEquals("Bilbo", userInfo.firstName());
+    Assertions.assertEquals("Baggings", userInfo.lastName());
+    Assertions.assertEquals("bilbo.baggings@gov.bc.ca", userInfo.email());
+    Assertions.assertEquals("Baggings, Bilbo LWRS:EX", userInfo.displayName());
+    Assertions.assertEquals("BAGGINGS", userInfo.idirUsername());
+    Assertions.assertNull(userInfo.businessName());
+    Assertions.assertEquals("idir", userInfo.identityProvider());
+    Assertions.assertTrue(userInfo.roles().isEmpty());
+  }
+}
