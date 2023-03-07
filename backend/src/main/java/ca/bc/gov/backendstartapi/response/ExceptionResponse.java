575a41e9727c4a171abe5113b68df75c6b86e5b2
Derek Roberts <derek.roberts@gmail.com>
2023-03-07 10:49:53 -0800

    Backend and related workflows (#13)
    * Vars, secrets
    
    * Fix backend structure
    
    * ci: autogenerated JaCoCo coverage badge
    
    ---------
    
    Co-authored-by: Ci Bot <cibot@users.noreply.github.com>

---
+++ backend/src/main/java/ca/bc/gov/backendstartapi/response/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package ca.bc.gov.backendstartapi.response;
+
+/** This class represents any kind of exception that this API may go through. */
+public record ExceptionResponse(String errorMessage) {}
