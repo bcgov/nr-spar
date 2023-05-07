b8ab0b22d1d1f2144927252589ce43a40116ab9d
Derek Roberts <derek.roberts@gmail.com>
2023-05-07 15:25:03 -0700

    Roll back 4x backend commits

---
+++ backend/src/main/java/ca/bc/gov/backendstartapi/entity/embeddable/AuditInformation.java
@@ -0,0 +1,55 @@
+package ca.bc.gov.backendstartapi.entity.embeddable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+/** Used to track the changes made in a registry. */
+@Embeddable
+@NoArgsConstructor
+@Getter
+public class AuditInformation implements Serializable {
+
+  @Generated @Serial private static final long serialVersionUID = -4611969329464135377L;
+
+  /** User that entered the registry in the system. */
+  @Column(name = "entry_userid", length = 30, nullable = false, updatable = false)
+  private String entryUserId;
+
+  @Column(name = "entry_timestamp", nullable = false, updatable = false)
+  private LocalDateTime entryTimestamp;
+
+  /** User who last updated this registry. */
+  @NonNull
+  @Setter
+  @Column(name = "update_userid", length = 30, nullable = false)
+  private String updateUserId;
+
+  @Column(name = "update_timestamp", nullable = false)
+  private LocalDateTime updateTimestamp;
+
+  public AuditInformation(@NonNull String userId) {
+    entryUserId = userId;
+    updateUserId = userId;
+  }
+
+  @PrePersist
+  private void prePersist() {
+    entryTimestamp = LocalDateTime.now();
+    updateTimestamp = entryTimestamp;
+  }
+
+  @PreUpdate
+  private void preUpdate() {
+    updateTimestamp = LocalDateTime.now();
+  }
+}
