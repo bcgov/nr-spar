1fe9824fd46f2b2424dcb550a747ab40b00079ef
ArthurEncr <arthur.alves@encora.com>
2023-05-10 10:55:10 -0300

    fix: removing eslint disable

--- frontend/src/__test__/components/TitleAccordion.test.tsx
+++ frontend/src/__test__/components/TitleAccordion.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import React from 'react'
 import { render, screen } from '@testing-library/react';
 import TitleAccordion from '../../components/TitleAccordion/index';
