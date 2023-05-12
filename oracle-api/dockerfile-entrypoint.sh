565c6f0fd62baf6e65d83d305545a15a484511ab
Annibal Silva <105941480+annibalsilva@users.noreply.github.com>
2023-05-11 21:14:24 -0300

    build: modify docker configuration for oracle-api (#108)
    * build: modify docker configuration for oracle-api
    
    * Reintroduced oracle-api optionally in Compose
    
    * Host networking
    
    ---------
    
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>

--- oracle-api/dockerfile-entrypoint.sh
+++ oracle-api/dockerfile-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-java --source 17 InstallCert.java --quiet "${DATABASE_HOST}:${DATABASE_PORT}"
+java -cp /usr/share/service/artifacts/ InstallCert --quiet "${DATABASE_HOST}:${DATABASE_PORT}"
 keytool -exportcert -alias "${DATABASE_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
 keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
-
-javac HealthCheck.java
 
 java \
     -Djava.security.egd=file:/dev/./urandom \
