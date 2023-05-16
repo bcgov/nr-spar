9f5efecf7f2a3f1e499cc8617d47e7b552b61f9e
Ricardo Campos <ricardo.campos@encora.com>
2023-05-16 10:13:14 -0300

    Tests: Cypress folder and test cases (#122)
    * fix: add cypress files
    
    * tests(cypress): add timeouts and cy origin to some test cases
    
    ---------
    
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>
    Co-authored-by: iatdaitan <86745196+iatdaitan@users.noreply.github.com>

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://nr-spar-webapp-test-frontend.apps.silver.devops.gov.bc.ca/',
+    baseUrl: 'http://localhost:3000/',
     viewportWidth: 1280,
     viewportHeight: 720,
     experimentalWebKitSupport: true,
     env: {
-      apiUrl: 'https://nrbestapi-test-service-api.apps.silver.devops.gov.bc.ca'
+      keycloakLoginUrl: 'https://logontest7.gov.bc.ca'
     },
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
