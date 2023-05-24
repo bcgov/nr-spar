16bddaf61ef95e7576ff317adf8fb6cfbfdffd12
Derek Roberts <derek.roberts@gmail.com>
2023-05-23 18:55:16 -0700

    Drop var REACT_APP_KC_REALM

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 import { env } from './env';
 
 const url = env.REACT_APP_KC_URL || '';
-const realm = env.REACT_APP_KC_REALM || '';
+const realm = env.REACT_APP_KC_REALM || 'standard';
 const clientId = env.REACT_APP_KC_CLIENT_ID || '';
 
 const keycloak = new Keycloak({
