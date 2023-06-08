65061d32a8d1fb864c0788dc665251e4f5dc25ec
mgaseta <matheus.gaseta@encora.com>
2023-06-08 16:31:55 -0300

    fix: fixing my seedlot page grid

--- frontend/src/views/Seedlot/MySeedlots/index.tsx
+++ frontend/src/views/Seedlot/MySeedlots/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import {
   Row,
+  Column,
   Breadcrumb,
   BreadcrumbItem,
   FlexGrid,
@@ -41,27 +42,31 @@ const MySeedlots = () => {
   }, []);
 
   return (
-    <FlexGrid>
+    <FlexGrid fullWidth className="my-seedlot-content">
       <Row className="my-seedlot-breadcrumb">
         <Breadcrumb>
           <BreadcrumbItem onClick={() => navigate('/seedlot')}>Seedlots</BreadcrumbItem>
         </Breadcrumb>
       </Row>
       <Row className="my-seedlot-title">
-        <PageTitle
-          title="My Seedlots"
-          subtitle="Check and manage my seedlots"
-          enableFavourite
-        />
-        <Button
-          kind="primary"
-          onClick={() => { navigate('/seedlot/register-a-class'); }}
-          size="md"
-          className="btn-my-seedlot"
-          renderIcon={Add}
-        >
-          Register a new seedlot
-        </Button>
+        <Column sm={4} md={6} lg={14} xlg={12}>
+          <PageTitle
+            title="My Seedlots"
+            subtitle="Check and manage my seedlots"
+            enableFavourite
+          />
+        </Column>
+        <Column sm={4} md={2} lg={2} xlg={4}>
+          <Button
+            kind="primary"
+            onClick={() => { navigate('/seedlot/register-a-class'); }}
+            size="lg"
+            className="btn-my-seedlot"
+            renderIcon={Add}
+          >
+            Register a new seedlot
+          </Button>
+        </Column>
       </Row>
       <Row className="my-seedlot-data-table-row">
         {seedlotsData.length > 0 && <SeedlotDataTable seedlots={seedlotsData} /> }
