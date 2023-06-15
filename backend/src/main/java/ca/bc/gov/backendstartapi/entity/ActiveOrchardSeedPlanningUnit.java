992e2f4d33a771944dfbdae8d4aaac9aca1d03cf
Ricardo Campos <ricardo.campos@encora.com>
2023-06-15 14:55:34 -0300

    fix: warnings on java code (#245)
    * fix: a bunch of warnings
    
    * chore: fix pom and checkstyle issues and warnings
    
    * fix: improve java instanceof list list code smell

--- backend/src/main/java/ca/bc/gov/backendstartapi/entity/ActiveOrchardSeedPlanningUnit.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/entity/ActiveOrchardSeedPlanningUnit.java
@@ -8,10 +8,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 /** Auxiliary entity connecting an Orchard and a Seed Plan Unit (SPU). */
@@ -19,7 +19,7 @@ import lombok.Setter;
 @Table(name = "active_orchard_spu")
 @IdClass(ActiveOrchardSeedPlanningUnitId.class)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Schema(description = "An association between an orchard and a Seed Plan Unit (SPU).")
@@ -32,18 +32,15 @@ public class ActiveOrchardSeedPlanningUnit {
 
   @Id
   @Column(name = "seed_plan_unit_id", nullable = false)
-  @NonNull
   private int seedPlanningUnitId;
 
   @Column(name = "active_ind", nullable = false)
-  @NonNull
   @Schema(
       description =
           "If this association is active; if `false`, it should not be used for new registries.")
   private boolean active;
 
   @Column(name = "retired_ind", nullable = false)
-  @NonNull
   @Schema(
       description =
           """
@@ -56,7 +53,6 @@ public class ActiveOrchardSeedPlanningUnit {
    * value will most likely be {@code -1}.
    */
   @Column(name = "no_spu_ind", nullable = false)
-  @NonNull
   @Schema(description = "If this orchard has never had a SPU assigned to it.")
   private boolean spuNotAssigned;
 }
