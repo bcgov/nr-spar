d69f575a0d2bef359804676f62d07b51804a275c
Ricardo Campos <ricardo.campos@encora.com>
2023-06-20 11:33:11 -0300

    tests: add more tests to orchard service

--- backend/src/test/java/ca/bc/gov/backendstartapi/service/OrchardServiceTest.java
+++ backend/src/test/java/ca/bc/gov/backendstartapi/service/OrchardServiceTest.java
@@ -3,11 +3,13 @@ package ca.bc.gov.backendstartapi.service;
 import static org.mockito.Mockito.when;
 
 import ca.bc.gov.backendstartapi.dto.OrchardParentTreeDto;
+import ca.bc.gov.backendstartapi.dto.ParentTreeDto;
+import ca.bc.gov.backendstartapi.dto.ParentTreeGeneticQualityDto;
 import ca.bc.gov.backendstartapi.entity.ActiveOrchardSeedPlanningUnit;
 import ca.bc.gov.backendstartapi.exception.NoSpuForOrchardException;
 import ca.bc.gov.backendstartapi.provider.OracleApiProvider;
 import ca.bc.gov.backendstartapi.repository.ActiveOrchardSeedPlanningUnitRepository;
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
@@ -84,8 +86,15 @@ class OrchardServiceTest {
     when(activeOrchardSeedPlanningUnitRepository.findByOrchardIdAndActive(orchardId, true))
         .thenReturn(List.of(activeOrchardSpu));
 
+    ParentTreeGeneticQualityDto geneticQualityDto =
+        new ParentTreeGeneticQualityDto("BV", "GVO", new BigDecimal("18.0"));
+
+    ParentTreeDto parentDto =
+        new ParentTreeDto(
+            123L, "37", "App", "123456", true, true, false, null, null, List.of(geneticQualityDto));
+
     OrchardParentTreeDto parentTreeDto =
-        new OrchardParentTreeDto(orchardId, "FDC", 1L, new ArrayList<>());
+        new OrchardParentTreeDto(orchardId, "FDC", 1L, List.of(parentDto));
 
     int spuId = 1;
 
@@ -98,6 +107,27 @@ class OrchardServiceTest {
     Assertions.assertEquals("405", orchardDto.orchardId());
     Assertions.assertEquals("FDC", orchardDto.vegetationCode());
     Assertions.assertEquals(1L, orchardDto.seedPlanningUnitId());
+
+    List<ParentTreeDto> parentTreeDtos = orchardDto.parentTrees();
+
+    Assertions.assertFalse(parentTreeDtos.isEmpty());
+    Assertions.assertEquals(123L, parentTreeDtos.get(0).parentTreeId());
+    Assertions.assertEquals("37", parentTreeDtos.get(0).parentTreeNumber());
+    Assertions.assertEquals("App", parentTreeDtos.get(0).parentTreeRegStatusCode());
+    Assertions.assertEquals("123456", parentTreeDtos.get(0).localNumber());
+    Assertions.assertTrue(parentTreeDtos.get(0).active());
+    Assertions.assertTrue(parentTreeDtos.get(0).tested());
+    Assertions.assertFalse(parentTreeDtos.get(0).breedingProgram());
+    Assertions.assertNull(parentTreeDtos.get(0).femaleParentTreeId());
+    Assertions.assertNull(parentTreeDtos.get(0).maleParentTreeId());
+
+    List<ParentTreeGeneticQualityDto> parentDtos =
+        parentTreeDtos.get(0).parentTreeGeneticQualities();
+
+    Assertions.assertFalse(parentDtos.isEmpty());
+    Assertions.assertEquals("BV", parentDtos.get(0).geneticTypeCode());
+    Assertions.assertEquals("GVO", parentDtos.get(0).geneticWorthCode());
+    Assertions.assertEquals(new BigDecimal("18.0"), parentDtos.get(0).geneticQualityValue());
   }
 
   @Test
