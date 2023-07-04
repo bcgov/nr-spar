252df2900ff1823e162ea8bbe6893f157b6f5fe7
Craig Yu <craig.yu93@gmail.com>
2023-07-03 17:20:38 -0700

    feat: getting rekt by java

--- backend/src/main/java/ca/bc/gov/backendstartapi/enums/MaleFemaleMethodologyEnum.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/enums/MaleFemaleMethodologyEnum.java
@@ -7,29 +7,35 @@ import io.swagger.v3.oas.annotations.media.Schema;
     description =
         "This object represents a method code to determine male/female gametic contribution.")
 public enum MaleFemaleMethodologyEnum implements DescribedEnum {
-  F1("Visual Estimate"),
-  F2("Measured Cone Volume"),
-  F3("Cone Weight"),
-  F4("Cone Number from Weight"),
-  F5("Cone Number from Standard Volume"),
-  F6("Sample of Seeds"),
-  F7("Filled Seeds"),
-  F8("Ramet Proportion by Clone"),
-  F9("Ramet Proportion by Age and Expected Production"),
-  M1("Portion of Ramets in Orchard"),
-  M2(" Pollen Volume Estimate by Partial Survey"),
-  M3("Pollen Volume Estimate by 100% Survey"),
-  M4("Ramet Proportion by Clone"),
-  M5("Ramet Proportion by Age and Expected Production");
+  F1("Visual Estimate", false),
+  F2("Measured Cone Volume", false),
+  F3("Cone Weight", false),
+  F4("Cone Number from Weight", false),
+  F5("Cone Number from Standard Volume", false),
+  F6("Sample of Seeds", false),
+  F7("Filled Seeds", false),
+  F8("Ramet Proportion by Clone", true),
+  F9("Ramet Proportion by Age and Expected Production", true),
+  M1("Portion of Ramets in Orchard", false),
+  M2("Pollen Volume Estimate by Partial Survey", false),
+  M3("Pollen Volume Estimate by 100% Survey", false),
+  M4("Ramet Proportion by Clone", true),
+  M5("Ramet Proportion by Age and Expected Production", true);
 
   private final String description;
+  private final Boolean isPLI;
 
-  MaleFemaleMethodologyEnum(String description) {
+  MaleFemaleMethodologyEnum(String description, Boolean isPLI) {
     this.description = description;
+    this.isPLI = isPLI;
   }
 
   @Override
   public String description() {
     return description;
+  }
+
+  public Boolean isPLI() {
+    return isPLI;
   }
 }
