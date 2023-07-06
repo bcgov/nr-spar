4bcef9c77a110ac0cc1e08f9df8eab105ffb6f06
Craig Yu <craig.yu93@gmail.com>
2023-07-06 16:06:45 -0700

    feat: ad isPLI to backend enum

--- backend/src/main/java/ca/bc/gov/backendstartapi/enums/MaleFemaleMethodologyEnum.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/enums/MaleFemaleMethodologyEnum.java
@@ -1,41 +1,58 @@
 package ca.bc.gov.backendstartapi.enums;
 
+import java.util.Optional;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.ToString;
 
-/** This enumeration represents a method code to determine male/female gametic contribution. */
-@Schema(
-    description =
-        "This object represents a method code to determine male/female gametic contribution.")
-public enum MaleFemaleMethodologyEnum implements DescribedEnum {
-  F1("Visual Estimate", false),
-  F2("Measured Cone Volume", false),
-  F3("Cone Weight", false),
-  F4("Cone Number from Weight", false),
-  F5("Cone Number from Standard Volume", false),
-  F6("Sample of Seeds", false),
-  F7("Filled Seeds", false),
-  F8("Ramet Proportion by Clone", true),
-  F9("Ramet Proportion by Age and Expected Production", true),
-  M1("Portion of Ramets in Orchard", false),
-  M2("Pollen Volume Estimate by Partial Survey", false),
-  M3("Pollen Volume Estimate by 100% Survey", false),
-  M4("Ramet Proportion by Clone", true),
-  M5("Ramet Proportion by Age and Expected Production", true);
+/**
+ * This enumeration represents a method code to determine male/female gametic
+ * contribution.
+ */
+@Getter
+@ToString
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@Schema(description = "This object represents a method code to determine male/female gametic contribution.")
+public enum MaleFemaleMethodologyEnum {
+  F1("F1", "Visual Estimate", false),
+  F2("F2", "Measured Cone Volume", false),
+  F3("F3", "Cone Weight", false),
+  F4("F4", "Cone Number from Weight", false),
+  F5("F5", "Cone Number from Standard Volume", false),
+  F6("F6", "Sample of Seeds", false),
+  F7("F7", "Filled Seeds", false),
+  F8("F8", "Ramet Proportion by Clone", true),
+  F9("F9", "Ramet Proportion by Age and Expected Production", true),
+  M1("M1", "Portion of Ramets in Orchard", false),
+  M2("M2", "Pollen Volume Estimate by Partial Survey", false),
+  M3("M3", "Pollen Volume Estimate by 100% Survey", false),
+  M4("M4", "Ramet Proportion by Clone", true),
+  M5("M5", "Ramet Proportion by Age and Expected Production", true);
 
+  private final String code;
   private final String description;
   private final Boolean isPLI;
 
-  MaleFemaleMethodologyEnum(String description, Boolean isPLI) {
+  MaleFemaleMethodologyEnum(String code, String description, Boolean isPLI) {
+    this.code = code;
     this.description = description;
     this.isPLI = isPLI;
   }
 
-  @Override
-  public String description() {
-    return description;
-  }
-
-  public Boolean isPLI() {
-    return isPLI;
+  /**
+   * Get a {@link MaleFemaleMethodologyEnum} by its code.
+   *
+   * @param code The code of the male/female methodology. E.g.: F1, F2, F3...
+   * @return An {@link Optional} of {@link MaleFemaleMethodologyEnum}
+   */
+  public static Optional<MaleFemaleMethodologyEnum> getByCode(String code) {
+    for (MaleFemaleMethodologyEnum methodology : values()) {
+      if (methodology.code.equals(code)) {
+        return Optional.of(methodology);
+      }
+    }
+    return Optional.empty();
   }
 }
