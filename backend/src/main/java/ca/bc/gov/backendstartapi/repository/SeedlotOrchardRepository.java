a1a1d80b64b81c1c848f14bd7ca4838a1ec79e88
Ricardo Campos <ricardo.campos@encora.com>
2023-07-10 18:40:07 -0300

    restoring files from main

--- backend/src/main/java/ca/bc/gov/backendstartapi/repository/SeedlotOrchardRepository.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/repository/SeedlotOrchardRepository.java
@@ -1,13 +1,8 @@
 package ca.bc.gov.backendstartapi.repository;
 
-import ca.bc.gov.backendstartapi.entity.seedlot.Seedlot;
 import ca.bc.gov.backendstartapi.entity.seedlot.SeedlotOrchard;
 import ca.bc.gov.backendstartapi.entity.seedlot.idclass.SeedlotOrchardId;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /** The repository for {@link SeedlotOrchard SeedlotOrchards}. */
-public interface SeedlotOrchardRepository extends JpaRepository<SeedlotOrchard, SeedlotOrchardId> {
-
-  List<SeedlotOrchard> findBySeedlot(Seedlot seedlot);
-}
+public interface SeedlotOrchardRepository extends JpaRepository<SeedlotOrchard, SeedlotOrchardId> {}
