5f6ae6e8382884c92cca1d1b18180607a3e3486a
Ricardo Campos <ricardo.campos@encora.com>
2023-07-20 18:49:03 -0300

    feat: gvo trait calculations - partial

--- backend/src/main/java/ca/bc/gov/backendstartapi/endpoint/GeneticWorthEndpoint.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/endpoint/GeneticWorthEndpoint.java
@@ -1,8 +1,15 @@
 package ca.bc.gov.backendstartapi.endpoint;
 
+import ca.bc.gov.backendstartapi.dto.GeneticWorthSummaryDto;
+import ca.bc.gov.backendstartapi.dto.GeneticWorthTraitsDto;
 import ca.bc.gov.backendstartapi.enums.GeneticWorthEnum;
+import ca.bc.gov.backendstartapi.service.GeneticWorthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "GeneticWorth")
 public class GeneticWorthEndpoint implements DescribedEnumEndpoint<GeneticWorthEnum> {
 
+  private final GeneticWorthService geneticWorthService;
+
+  GeneticWorthEndpoint(GeneticWorthService geneticWorthService) {
+    this.geneticWorthService = geneticWorthService;
+  }
+
   @Override
   public Class<GeneticWorthEnum> enumClass() {
     return GeneticWorthEnum.class;
+  }
+
+  @PostMapping(path = "/calculate-file", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public GeneticWorthSummaryDto geneticTraitsCalculations(@RequestBody List<GeneticWorthTraitsDto> traitsDto) {
+    return geneticWorthService.calculateGeneticWorth(traitsDto);
   }
 }
