7f4ccf5c3e293697c6b160c84710b65fd135b93b
mgaseta <matheus.gaseta@encora.com>
2023-07-24 14:23:11 -0300

    feat: creating mock api for applicant agencies

---
+++ frontend/src/mock-server/fixtures/ApplicantAgenciesItems.ts
@@ -0,0 +1,51 @@
+import ApplicantAgencyType from '../../types/ApplicantAgencyType';
+
+const ApplicantAgenciesItems: ApplicantAgencyType[] = [
+  {
+    clientNumber: '00000002',
+    clientName: 'BAXTER',
+    legalFirstName: 'JAMES',
+    legalMiddleName: 'Canter',
+    clientStatusCode: 'ACT',
+    clientTypeCode: 'I',
+    acronym: 'JAMES BAXTER'
+  },
+  {
+    clientNumber: '00000002',
+    clientName: 'BAXTER',
+    legalFirstName: 'JAMES',
+    legalMiddleName: 'Canter',
+    clientStatusCode: 'ACT',
+    clientTypeCode: 'I',
+    acronym: 'JAMES BAXTER'
+  },
+  {
+    clientNumber: '00000002',
+    clientName: 'BAXTER',
+    legalFirstName: 'JAMES',
+    legalMiddleName: 'Canter',
+    clientStatusCode: 'ACT',
+    clientTypeCode: 'I',
+    acronym: 'JAMES BAXTER'
+  },
+  {
+    clientNumber: '00000002',
+    clientName: 'BAXTER',
+    legalFirstName: 'JAMES',
+    legalMiddleName: 'Canter',
+    clientStatusCode: 'ACT',
+    clientTypeCode: 'I',
+    acronym: 'JAMES BAXTER'
+  },
+  {
+    clientNumber: '00000002',
+    clientName: 'BAXTER',
+    legalFirstName: 'JAMES',
+    legalMiddleName: 'Canter',
+    clientStatusCode: 'ACT',
+    clientTypeCode: 'I',
+    acronym: 'JAMES BAXTER'
+  }
+];
+
+export default ApplicantAgenciesItems;
