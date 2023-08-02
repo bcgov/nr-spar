63ab22dc03b9b965f5916335fbc244a9c72f18a6
Ricardo Campos <ricardo.campos@encora.com>
2023-08-02 17:45:26 -0300

    tests: add genetic worth service tests

--- backend/src/main/java/ca/bc/gov/backendstartapi/entity/GeneticWorth.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/entity/GeneticWorth.java
@@ -4,13 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /** This class represents a Genetic Worth object in the database. */
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "genetic_worth")
 public class GeneticWorth {
   
