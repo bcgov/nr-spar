fed516922d1730c2191d731095f9f464a4e8a7aa
Craig Yu <craig.yu93@gmail.com>
2023-08-14 13:06:20 -0700

    fix: remove unused lines

--- backend/src/main/java/ca/bc/gov/backendstartapi/config/SwaggerConfig.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package ca.bc.gov.backendstartapi.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -66,9 +65,6 @@ public class SwaggerConfig {
     openApi.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     openApi.setComponents(components);
 
-    // NEXT: find a way to keep this closer to the controllers themselves.
-    Paths paths = new Paths();
-    openApi.setPaths(paths);
     return openApi;
   }
 }
