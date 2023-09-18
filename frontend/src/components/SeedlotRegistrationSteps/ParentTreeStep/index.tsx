2a6d720cba4a570ddf2bff9de5bed358c77fe543
Craig Yu <craig.yu93@gmail.com>
2023-09-18 13:31:21 -0700

    fix: remove eslint comment

--- frontend/src/components/SeedlotRegistrationSteps/ParentTreeStep/index.tsx
+++ frontend/src/components/SeedlotRegistrationSteps/ParentTreeStep/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useRef } from 'react';
 import { AxiosError, AxiosResponse } from 'axios';
 import { Link } from 'react-router-dom';
