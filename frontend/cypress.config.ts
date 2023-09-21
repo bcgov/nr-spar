c63fc9fec24e70e0917c882e4a4e194533a15460
Craig Yu <craig.yu93@gmail.com>
2023-09-21 11:30:38 -0700

    feat: cypress improvements (#488)
    * feat: cypress improvements
    
    * package-lock.json
    
    * fix: update snapshot
    
    * fix: replace package-lock
    
    * Revert "package-lock.json"
    
    This reverts commit 5f4e03ad7fb509baab2ee3f096b2277331f852f5.
    
    * fix: clear everything before login

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -9,6 +9,14 @@ export default defineConfig({
     env: {
       keycloakLoginUrl: 'https://logontest7.gov.bc.ca'
     },
+    specPattern: [
+      '**/login-page.cy.ts',
+      '**/dashboard-page.cy.ts',
+      '**/seedlot-main-page.cy.ts',
+      '**/aclass-seedlot-registration.cy.ts'
+    ],
+    // testIsolation: false,
+    chromeWebSecurity: false,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
