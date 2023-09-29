8891b38421d342183f5bca3645827e051ead2777
Ricardo Campos <ricardo.campos@encora.com>
2023-09-29 15:28:06 -0300

    feat: add runtime hints for cloud native
    Issue #480

--- backend/src/main/java/ca/bc/gov/backendstartapi/config/HttpServletRequestRuntimeHint.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/config/HttpServletRequestRuntimeHint.java
@@ -4,10 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.aot.hint.ProxyHints;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.context.annotation.Configuration;
 
 /** This class creates a RuntimeHint for the ServletRequest class. */
-@Configuration
 public class HttpServletRequestRuntimeHint implements RuntimeHintsRegistrar {
 
   @Override
