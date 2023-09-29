4a9395ecbb57f4b2bfdca9aa69f963be6486f5fb
Ricardo Campos <ricardo.campos@encora.com>
2023-09-29 15:02:08 -0300

    feat: add runtime hint for http servlet request
    Issue #480
    
    Runtime Hints required for graal vm and cloud native.

---
+++ backend/src/main/java/ca/bc/gov/backendstartapi/config/HttpServletRequestRuntimeHint.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.backendstartapi.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.aot.hint.ProxyHints;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpServletRequestRuntimeHint implements RuntimeHintsRegistrar {
+
+  @Override
+  public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+    try {
+      ProxyHints proxies = hints.proxies();
+      proxies.registerJdkProxy(HttpServletRequest.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Could not register RuntimeHint: " + e.getMessage());
+    }
+  }
+}
