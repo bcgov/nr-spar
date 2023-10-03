8e4f19662456e681bc0dd6be147a7a610c6db8bb
Derek Roberts <derek.roberts@gmail.com>
2023-10-03 11:37:16 -0700

    VITE_AWS_DOMAIN try 2

--- frontend/src/aws-exports.ts
+++ frontend/src/aws-exports.ts
@@ -1,11 +1,11 @@
 import { env } from './env';
 
 // VITE_ZONE: One of dev | test | prod
-const ZONE = env.VITE_ZONE? env.VITE_ZONE.toLocaleLowerCase() : 'dev';
-const retUrlEnv = ZONE !== 'prod' && ZONE !== 'test'? 'dev' : ZONE;
+const ZONE = env.VITE_ZONE ? env.VITE_ZONE.toLocaleLowerCase() : 'dev';
+const retUrlEnv = ZONE !== 'prod' && ZONE !== 'test' ? 'dev' : ZONE;
 
 // [https://logon7.gov.bc.ca] for PROD and [https://logontest7.gov.bc.ca] for everyting else
-const logoutDomain = ZONE === 'prod'? 'https://logon7.gov.bc.ca' : 'https://logontest7.gov.bc.ca';
+const logoutDomain = ZONE === 'prod' ? 'https://logon7.gov.bc.ca' : 'https://logontest7.gov.bc.ca';
 
 const signOutUrl = [
   `${logoutDomain}/clp-cgi/logoff.cgi`,
@@ -15,18 +15,18 @@ const signOutUrl = [
 ].join('');
 
 const awsconfig = {
-    aws_cognito_region: env.VITE_COGNITO_REGION || "ca-central-1",
-    aws_user_pools_id: env.VITE_USER_POOLS_ID,
-    aws_user_pools_web_client_id: env.VITE_USER_POOLS_WEB_CLIENT_ID,
-    aws_mandatory_sign_in: 'enable',
-    oauth: {
-        domain: env.VITE_AWS_DOMAIN || "prod-fam-user-pool-domain.auth.ca-central-1.amazoncognito.com",
-        scope: ['openid'],
-        redirectSignIn: `${window.location.origin}/dashboard`,
-        redirectSignOut: env.VITE_REDIRECT_SIGN_OUT || signOutUrl,
-        responseType: 'code',
-    },
-    federationTarget: 'COGNITO_USER_POOLS',
+  aws_cognito_region: env.VITE_COGNITO_REGION || 'ca-central-1',
+  aws_user_pools_id: env.VITE_USER_POOLS_ID,
+  aws_user_pools_web_client_id: env.VITE_USER_POOLS_WEB_CLIENT_ID,
+  aws_mandatory_sign_in: 'enable',
+  oauth: {
+    domain: env.VITE_AWS_DOMAIN || 'prod-fam-user-pool-domain.auth.ca-central-1.amazoncognito.com',
+    scope: ['openid'],
+    redirectSignIn: `${window.location.origin}/dashboard`,
+    redirectSignOut: env.VITE_REDIRECT_SIGN_OUT || signOutUrl,
+    responseType: 'code'
+  },
+  federationTarget: 'COGNITO_USER_POOLS'
 };
 
 export default awsconfig;
