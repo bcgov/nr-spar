7b64d7cf017d33d1c5608e73c504e6c120931dcf
Craig Yu <craig.yu93@gmail.com>
2023-10-26 12:58:43 -0700

    fix: use bool

--- frontend/src/components/ApplicantInformationForm/index.tsx
+++ frontend/src/components/ApplicantInformationForm/index.tsx
@@ -150,7 +150,7 @@ const ApplicantInformationForm = () => {
         locationCode: {
           ...prevResBody.locationCode,
           value: formattedCode,
-          isInvalid: !isInRange
+          isInvalid: false
         }
       }));
     }
