e08d7697cb7c29bf0d9a5cbd54cb2cea84dc40bb
Craig Yu <craig.yu93@gmail.com>
2023-12-05 10:02:46 -0800

    feat: refactor types

--- frontend/src/components/SeedlotRegistrationSteps/OrchardStep/definitions.ts
+++ frontend/src/components/SeedlotRegistrationSteps/OrchardStep/definitions.ts
@@ -1,17 +1,19 @@
+import { StringInputType } from '../../../types/FormInputType';
 import MultiOptionsObj from '../../../types/MultiOptionsObject';
 
 export type OrchardObj = {
   inputId: number,
   selectedItem: MultiOptionsObj | null
+  isInvalid: boolean
 }
 
 export type OrchardForm = {
   orchards: Array<OrchardObj>,
-  femaleGametic: string,
-  maleGametic: string,
+  femaleGametic: StringInputType,
+  maleGametic: StringInputType,
   controlledCross: boolean,
   biotechProcess: boolean,
   noPollenContamination: boolean,
-  breedingPercentage: string,
+  breedingPercentage: StringInputType,
   pollenMethodology: boolean
 }
