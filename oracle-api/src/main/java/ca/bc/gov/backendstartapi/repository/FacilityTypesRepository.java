093fa3185fc1ff2961cd31833146ef708591fab0
mgaseta <matheus.gaseta@encora.com>
2024-01-03 08:51:55 -0300

    feat: adding repository and endpoint tests and other minor fixes

--- oracle-api/src/main/java/ca/bc/gov/backendstartapi/repository/FacilityTypesRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/backendstartapi/repository/FacilityTypesRepository.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-/** This interface enables the funding source entity to be retrieved from the database. */
+/** This interface enables the facility type entity to be retrieved from the database. */
 public interface FacilityTypesRepository extends JpaRepository<FacilityTypes, String> {
 
   @Query(
       value =
-          "select ft from FacilityType ft WHERE CURRENT_DATE >= ft.effectiveDate "
+          "select ft from FacilityTypes ft WHERE CURRENT_DATE >= ft.effectiveDate "
               + "AND CURRENT_DATE < ft.expiryDate ORDER BY ft.code")
   List<FacilityTypes> findAllValid();
 }
