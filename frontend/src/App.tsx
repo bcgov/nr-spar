59d33458cd8e565e0a8f2290f300d7a32437b54b
Ricardo Campos <ricardo.campos@encora.com>
2024-01-03 13:53:39 -0300

    fix: remove unused code
    issue #568

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -132,7 +132,6 @@ const App: React.FC = () => {
                     </ProtectedRoute>
                   )}
                 />
-                <Route path="/downloads/*" />
                 <Route path="*" element={<Navigate to="/dashboard" replace />} />
               </Route>
               <Route path="/404" element={<FourOhFour />} />
