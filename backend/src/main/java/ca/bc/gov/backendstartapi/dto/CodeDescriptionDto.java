b60640c6524f14dc2c0860aa2819c748691b57d9
Ricardo Campos <ricardo.campos@encora.com>
2024-01-15 13:52:30 -0300

    feat: improve genetic class endpoint logs
    issue #326

--- backend/src/main/java/ca/bc/gov/backendstartapi/dto/CodeDescriptionDto.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/dto/CodeDescriptionDto.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.backendstartapi.dto;
 
+import ca.bc.gov.backendstartapi.entity.GeneticClassEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
@@ -14,4 +15,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record CodeDescriptionDto(
     @Schema(description = "The Code that represent a data object", example = "1") String code,
     @Schema(description = "The description/value of the data object", example = "Squirrel cache")
-        String description) {}
+        String description) {
+  
+  /**
+   * Creates a {@link CodeDescriptionDto} from a {@link GeneticClassEntity} instance.
+   *
+   * @param entity The GeneticClassEntity instance
+   * @return A CodeDescriptionDto
+   */
+  public static CodeDescriptionDto fromEntity(GeneticClassEntity entity) {
+    return new CodeDescriptionDto(entity.getGeneticClassCode(), entity.getDescription());
+  }
+}
