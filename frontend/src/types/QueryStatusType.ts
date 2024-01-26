d01a09f3f4ee78856e37f81f2f694d485f6556b8
Craig Yu <craig.yu93@gmail.com>
2024-01-25 21:16:35 -0800

    feat: stash buggy code

---
+++ frontend/src/types/QueryStatusType.ts
@@ -0,0 +1,1 @@
+export type QueryStatusType = 'error' | 'loading' | 'success';
--- frontend/src/views/Seedlot/SeedlotDetails/FormProgress/index.tsx
+++ frontend/src/views/Seedlot/SeedlotDetails/FormProgress/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 import {
@@ -6,24 +8,73 @@ import {
 } from '@carbon/react';
 import { Edit } from '@carbon/icons-react';
 
+import { getAClassSeedlotProgressStatus } from '../../../../api-service/seedlotAPI';
+import { ProgressIndicatorConfig } from '../../SeedlotRegistrationForm/definitions';
 import SeedlotRegistrationProgress from '../../../../components/SeedlotRegistrationProgress';
-import { initialProgressConfig } from '../../SeedlotRegistrationForm/constants';
+import { completeProgressConfig, initialProgressConfig } from '../../SeedlotRegistrationForm/constants';
 import PathConstants from '../../../../routes/pathConstants';
 import { addParamToPath } from '../../../../utils/PathUtils';
+import { QueryStatusType } from '../../../../types/QueryStatusType';
+import { SeedlotStatusCode } from '../../../../types/SeedlotType';
 
 import './styles.scss';
 
 interface FormProgressProps {
   seedlotNumber?: string;
-  isFetching?: boolean;
+  seedlotStatusCode?: SeedlotStatusCode;
+  getSeedlotQueryStatus: QueryStatusType
 }
 
-const FormProgress = ({ seedlotNumber, isFetching }: FormProgressProps) => {
+const FormProgress = (
+  {
+    seedlotNumber,
+    seedlotStatusCode,
+    getSeedlotQueryStatus
+  }: FormProgressProps
+) => {
   const navigate = useNavigate();
 
-  if (!seedlotNumber) {
-    return null;
-  }
+  const [
+    progressStatus,
+    setProgressStatus
+  ] = useState<ProgressIndicatorConfig>(initialProgressConfig);
+
+  useEffect(() => {
+    if (seedlotStatusCode && seedlotStatusCode !== 'PND' && seedlotStatusCode !== 'INC') {
+      setProgressStatus(completeProgressConfig);
+    }
+  }, [seedlotStatusCode]);
+
+  const formProgressStatusQuery = useQuery({
+    queryKey: ['seedlots', 'a-class-form-progress', 'status', seedlotNumber],
+    queryFn: () => getAClassSeedlotProgressStatus(seedlotNumber ?? ''),
+    enabled: getSeedlotQueryStatus === 'success'
+      && (seedlotStatusCode === 'PND' || seedlotStatusCode === 'INC'),
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    retry: 1
+  });
+
+  useEffect(() => {
+    if (formProgressStatusQuery.status === 'success') {
+      const retreivedProgress = formProgressStatusQuery.data
+        .data as unknown as ProgressIndicatorConfig;
+
+      const keys = Object.keys(retreivedProgress) as (keyof ProgressIndicatorConfig)[];
+
+      keys.forEach((key) => {
+        retreivedProgress[key].isCurrent = false;
+      });
+
+      setProgressStatus(formProgressStatusQuery.data.data as unknown as ProgressIndicatorConfig);
+    }
+
+    if (formProgressStatusQuery.status === 'error') {
+      if ((formProgressStatusQuery.error as AxiosError).response?.status === 404) {
+        setProgressStatus(initialProgressConfig);
+      }
+    }
+  }, [seedlotStatusCode, formProgressStatusQuery.status]);
 
   return (
     <div className="form-progress">
@@ -32,22 +83,28 @@ const FormProgress = ({ seedlotNumber, isFetching }: FormProgressProps) => {
           See where you are in the registration process
         </p>
       </div>
-      <div className="steps-box">
-        {
-          isFetching
-            ? <ProgressIndicatorSkeleton />
-            : (
-              <SeedlotRegistrationProgress
-                progressStatus={initialProgressConfig}
-                interactFunction={(e: number) => {
-                  // Add 1 to the number to make it comply with
-                  // the step numbers shown to the user
-                  navigate(`${addParamToPath(PathConstants.SEEDLOT_A_CLASS_REGISTRATION, seedlotNumber ?? '')}?step=${e + 1}`);
-                }}
-              />
-            )
-        }
-      </div>
+      {
+        seedlotStatusCode !== undefined
+          ? (
+            <div className="steps-box">
+              {
+                (seedlotStatusCode === 'PND' || seedlotStatusCode === 'INC')
+                  && (getSeedlotQueryStatus === 'loading' || formProgressStatusQuery.status === 'loading')
+                  ? <ProgressIndicatorSkeleton />
+                  : (
+                    <SeedlotRegistrationProgress
+                      progressStatus={progressStatus}
+                      interactFunction={(e: number) => {
+                        // Add 1 to the number to make it comply with
+                        // the step numbers shown to the user
+                        navigate(`${addParamToPath(PathConstants.SEEDLOT_A_CLASS_REGISTRATION, seedlotNumber ?? '')}?step=${e + 1}`);
+                      }}
+                    />
+                  )
+              }
+            </div>
+          ) : null
+      }
       <div>
         <Button
           kind="tertiary"
@@ -55,7 +112,7 @@ const FormProgress = ({ seedlotNumber, isFetching }: FormProgressProps) => {
           className="btn-fp"
           renderIcon={Edit}
           onClick={() => navigate(addParamToPath(PathConstants.SEEDLOT_A_CLASS_REGISTRATION, seedlotNumber ?? ''))}
-          disabled={isFetching || !seedlotNumber}
+          disabled={getSeedlotQueryStatus === 'loading'}
         >
           Edit seedlot form
         </Button>
