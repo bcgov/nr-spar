8a54b891492535b3e5321eeecf4701458d811e70
ArthurEncr <arthur.alves@encora.com>
2024-01-26 11:27:29 -0300

    fix: eslint error fixes

--- frontend/src/components/BCBreadcrumb/index.tsx
+++ frontend/src/components/BCBreadcrumb/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -13,66 +15,64 @@ interface BCBreadcrumbProps {
     {
       name: string,
       path: string
-    }[]  
+    }[];
 }
 
-const BCBreadcrumb = ({breadcrumbData}:BCBreadcrumbProps) => {
+const BCBreadcrumb = ({ breadcrumbData }:BCBreadcrumbProps) => {
   const navigate = useNavigate();
 
-  const lastBreadcrumb = breadcrumbData.length - 1
-  const overflowData = breadcrumbData.slice(1, lastBreadcrumb)
+  const lastBreadcrumb = breadcrumbData.length - 1;
+  const overflowData = breadcrumbData.slice(1, lastBreadcrumb);
 
   if (breadcrumbData.length >= 3) {
     return (
-    <div className="breadcrumbs-container">
-      <Breadcrumb>
-        <BreadcrumbItem onClick={() => navigate(breadcrumbData[0].path)}>
-            {breadcrumbData[0].name}
-        </BreadcrumbItem>
-        {overflowData.map((breadcrumb) => (
-          <BreadcrumbItem 
-            onClick={() => navigate(breadcrumb.path)} 
-            className="overflow-breadcrumb"
-            key={breadcrumb.name}
-          >
-            {breadcrumb.name}
-          </BreadcrumbItem>
-      ))}
-        <BreadcrumbItem className="overflow-menu-container">
-          <OverflowMenu>
-            {overflowData.map((breadcrumb) => (
-              <BreadcrumbItem 
-                onClick={() => navigate(breadcrumb.path)} 
-                key={breadcrumb.name}
-              >
-                {breadcrumb.name}
-              </BreadcrumbItem>
-          ))}
-          </OverflowMenu>
-        </BreadcrumbItem>
-        <BreadcrumbItem onClick={() => navigate(breadcrumbData[lastBreadcrumb].path)}>
-            {breadcrumbData[lastBreadcrumb].name}
-        </BreadcrumbItem>
-      </Breadcrumb>
-    </div>
-  )}
-  else{
-    return(
       <div className="breadcrumbs-container">
         <Breadcrumb>
-          {breadcrumbData.map((breadcrumb) => (
-            <BreadcrumbItem 
-              onClick={() => navigate(breadcrumb.path)} 
+          <BreadcrumbItem onClick={() => navigate(breadcrumbData[0].path)}>
+            {breadcrumbData[0].name}
+          </BreadcrumbItem>
+          {overflowData.map((breadcrumb) => (
+            <BreadcrumbItem
+              onClick={() => navigate(breadcrumb.path)}
+              className="overflow-breadcrumb"
               key={breadcrumb.name}
             >
               {breadcrumb.name}
             </BreadcrumbItem>
-        ))}
+          ))}
+          <BreadcrumbItem className="overflow-menu-container">
+            <OverflowMenu>
+              {overflowData.map((breadcrumb) => (
+                <BreadcrumbItem
+                  onClick={() => navigate(breadcrumb.path)}
+                  key={breadcrumb.name}
+                >
+                  {breadcrumb.name}
+                </BreadcrumbItem>
+              ))}
+            </OverflowMenu>
+          </BreadcrumbItem>
+          <BreadcrumbItem onClick={() => navigate(breadcrumbData[lastBreadcrumb].path)}>
+            {breadcrumbData[lastBreadcrumb].name}
+          </BreadcrumbItem>
         </Breadcrumb>
       </div>
-    )
+    );
   }
-  ;
+  return (
+    <div className="breadcrumbs-container">
+      <Breadcrumb>
+        {breadcrumbData.map((breadcrumb) => (
+          <BreadcrumbItem
+            onClick={() => navigate(breadcrumb.path)}
+            key={breadcrumb.name}
+          >
+            {breadcrumb.name}
+          </BreadcrumbItem>
+        ))}
+      </Breadcrumb>
+    </div>
+  );
 };
 
 export default BCBreadcrumb;
