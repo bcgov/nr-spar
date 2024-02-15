f8361e38aa4a09cc496554049a0f46de40b18f7e
marcionemec-daitan <83302904+marcionemec-daitan@users.noreply.github.com>
2024-02-15 11:38:36 -0300

    Develop MR to Main, bringing vault unit tests (#32)
    * Adapt data sync engine to run on jenkins (#18)
    
    #5: Adapt Engine to run on Jenkins Cron
    
    * #19 Include Compliance YAML to project to track risks and Privacy Impact Assessment status (#21)
    
    * #13 Include Badge for project Status (#24)
    
    * Feature/27/marcio/jenkins convert to polaris (#28)
    
    * #27 - Including JenkinsFile for Polaris Jenkins Server
    
    * #27 - Including some changes to test Polaris Features
    
    * #27 Updating agent configuration
    
    * #27 Remove unnecessary python checking
    
    * #27 Including post conditions
    
    * #27 Including 1st test to determine readiness of code
    
    * #27 Fixing missing single quote
    
    * #27 pwd + dir
    
    * #27 Including Docker file for podman build containerization
    
    * #27 Hello World message to guarantee no error message in pipeline, for now.
    
    * Feature/30/marcio/vault credentials to jenkins polaris (#31)
    
    * #27 - Including JenkinsFile for Polaris Jenkins Server
    
    * #27 - Including some changes to test Polaris Features
    
    * #27 Updating agent configuration
    
    * #27 Remove unnecessary python checking
    
    * #27 Including post conditions
    
    * #27 Including 1st test to determine readiness of code
    
    * #27 Fixing missing single quote
    
    * #27 pwd + dir
    
    * #27 Including Docker file for podman build containerization
    
    * #27 Hello World message to guarantee no error message in pipeline, for now.
    
    * #30 Testing Vault Access with Token
    
    * #30 Vtoken credential as environment variable for all stages
    
    * #30 Including requests in the libraries of project
    
    * #30 passing environment arguments to container
    
    * #30 removing JSON from requirements.txt
    
    * #30 Handling env vars
    
    * #30 Bringing secrets inside podman
    
    * #30 Fixing arguments syntax
    
    * #30 fixing typo
    
    * #30 Using Podman polaris library to build container
    
    * #30 removing script podman library exchanging to execute withcredentials
    
    * #30 Fixing credentials getter
    
    * #30 fixing env var names
    
    * #30 fixing env var passthrough pod
    
    * #30 Including Unit Test for env Variables
    
    * #30 fixing identation
    
    * #30 renaming env var
    
    * #30 renaming env vars
    
    * #30 renaming env jenkins vars
    
    * #30 renaming variables
    
    * #30 removing cred for simple vars
    
    * #30 Quote env vars from jenkins
    
    * #30 Verifying env vars
    
    * #30 fixing env var
    
    * #30 sync vars env
    
    * #30 change env vars to bring req jenkins vars
    
    * #30 Solve Boolean type mismatch
    
    * #30 fixing library os environ access
    
    * #30 fixing unit test function
    
    * #30 vault token compliance unit test

--- src/main.py
+++ src/main.py
@@ -1,22 +1,71 @@
+import requests
+import json
 import module.data_synchronization as data_sync
 from logging import config as logging_config
 import os
+import sys
 
-def do_tests_in_jenkins():
-    print("Starting tests")
+def required_variables_exists():
+    ret = True
+    print("Checking if required variables are defined")
     
-    # Testing Secrets variable from Jenkins Credentials Plugin    
-    print(os.environ['Usr_teste'] + " " + os.environ['Psw_Test'] ) 
-    
-    if os.environ['Usr_teste'] == "usuario_teste":
-        print("Usr credential accepted")
-    else:
-        print("Usr credential incorrect")
+    if os.environ.get("teste") is None:
+        print("Error: teste variable is None")
+        ret = False
         
-    if os.environ['Psw_Test'] == "senha":
-        print("Password credential accepted")
+    if os.environ.get("vtoken") is None:
+        print("Error: vtoken variable is None")
+        ret = False
+        
+    if os.environ.get("vurl") is None:
+        print("Error: vurl variable is None")
+        ret = False
+        
+    if os.environ.get("test_mode") is None:
+        print("Error: test mode variable is None")
+        ret = False
+        
+    if ret:
+        print("Required variable test passed!")
     else:
-        print("Password credential incorrect")
+        raise Exception("Not all required variables to execute a instance of Data Sync Engine exists.")
+    
+        
+def testVault():  
+    ret = True
+    
+    teste = os.environ['teste']  # Copying my token from vault
+    if teste =='123':
+        print("Test control variable is ok")
+    else:
+        ret = False
+        print("Test Control variable value is not expected")
+        
+    vault_url = os.environ['vurl']  # Vault url
+    if vault_url.startswith('https://'):
+        print("Vault URL looks good")
+    else:
+        ret = False
+        print("Vault URL value is not expected")
+    
+    vault_token = os.environ['vtoken']  # Copying my token from vault
+    if vault_token.startswith('hvs.'):
+        print("Vault token variable looks good (it not means token is correct)")
+    else:
+        ret = False
+        print("Vault token value is not in the pattern requested")
+    
+    if ret:
+        vault_url = 'https://knox.io.nrs.gov.bc.ca/v1/groups/data/spar/test'
+        headers = {'X-Vault-Token': vault_token}
+        res = requests.get(vault_url, headers=headers)
+        # print(res.text)    
+        j = json.loads(res.text)
+        # print(j)
+        
+    else:
+        print("Vault cannot be reached as required variables are not correctly informed")
+
 
 def main() -> None:
     logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
@@ -24,5 +73,13 @@ def main() -> None:
     data_sync.data_sync()    
     
 if __name__ == '__main__':
-    print("I'm containerized now. So what?")
-    #main()
+    definitiion_of_yes = ["Y","YES","1","T","TRUE"]
+    this_is_a_test = sys.argv[1]
+    if this_is_a_test in definitiion_of_yes:
+        print("Executing in Test mode")
+        required_variables_exists()
+        testVault()
+        
+    else:
+        main()
+   
