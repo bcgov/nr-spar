5fe4510e4d3ec78e8bd4e15a62287d60c4bc77b7
Craig Yu <craig.yu93@gmail.com>
2024-03-21 15:47:52 -0700

    Revert "Feat/916 cypress test on seedlot detail page (#919)" (#923)
    This reverts commit 423139ffcfccd1ef175291a91ea66b7b76e03a80.

--- frontend/cypress/e2e/smoke-test/seedlot-main-page.cy.ts
+++ frontend/cypress/e2e/smoke-test/seedlot-main-page.cy.ts
@@ -30,33 +30,4 @@ describe('Seedlot Main page test', () => {
       .find('.recent-seedlots-subtitle')
       .should('have.text', seedlotMainPageData.secondSectionSubtitle);
   });
-
-  it('should Check if 4 seedlots are being rendered at the bottom', () => {
-    cy.get('table.bx--data-table')
-      .should('be.visible')
-    
-    cy.get('table.bx--data-table tbody tr')
-      .eq(0)
-      .find('td')
-      .first()
-      .should('have.text', '63001');
-
-    cy.get('table.bx--data-table tbody tr')
-      .eq(1)
-      .find('td')
-      .first()
-      .should('have.text', '63032');
-
-    cy.get('table.bx--data-table tbody tr')
-      .eq(2)
-      .find('td')
-      .first()
-      .should('have.text', '63001');
-
-    cy.get('table.bx--data-table tbody tr')
-      .eq(3)
-      .find('td')
-      .first()
-      .should('have.text', '63012');
-  });
 });
