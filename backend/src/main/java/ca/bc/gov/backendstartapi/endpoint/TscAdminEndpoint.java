93db960f22bc950fca3506caa60ff18c33170ce9
Ricardo Campos <ricardo.campos@encora.com>
2024-03-27 17:59:49 -0300

    test: add tests for the tsc admin seedlot fetching api
    issue #894

--- backend/src/main/java/ca/bc/gov/backendstartapi/endpoint/TscAdminEndpoint.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/endpoint/TscAdminEndpoint.java
@@ -1,41 +1,44 @@
 package ca.bc.gov.backendstartapi.endpoint;
 
-import ca.bc.gov.backendstartapi.config.SparLog;
 import ca.bc.gov.backendstartapi.entity.seedlot.Seedlot;
-import ca.bc.gov.backendstartapi.security.UserAuthenticationHelper;
 import ca.bc.gov.backendstartapi.service.TscAdminService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/** This class contains resources for handling TSC Admin role requests. */
 @RequestMapping("/api/tsc-admin")
 @RestController
 @RequiredArgsConstructor
 @Tag(
     name = "TSC Admin",
-    description = "This resource contains resources for handling TSC Admin role requests.")
+    description = "This API contains resources for handling TSC Admin role requests.")
 public class TscAdminEndpoint {
 
   private final TscAdminService tscAdminService;
 
-  private final UserAuthenticationHelper userAuthenticationHelper;
-
+  /**
+   * Fetch all Seedlots for the TSC Admin role.
+   *
+   * @param page Optional page, zero-based, default 0.
+   * @param size Optional page size, default 10.
+   * @return A list of Seedlot or an empty list.
+   */
   @GetMapping("/seedlots")
   @PreAuthorize("hasRole('SPAR_SPR_TREE_SEED_CENTRE_ADMIN')")
-  @CrossOrigin(exposedHeaders = "X-TOTAL-COUNT")
   @Operation(
       summary = "Fetch all Seedlots for the TSC Admin role.",
       description =
@@ -49,20 +52,21 @@ public class TscAdminEndpoint {
             description = "Access token is missing or invalid",
             content = @Content(schema = @Schema(implementation = Void.class)))
       })
-  public ResponseEntity<Page<Seedlot>> getSeedlotsForReviewing(
-      @RequestParam(value = "page", required = false, defaultValue = "0") int page,
-      @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+  public ResponseEntity<List<Seedlot>> getSeedlotsForReviewing(
+      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+      @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
     Page<Seedlot> pageResult = tscAdminService.getTscAdminSeedlots(page, size);
     String totalCount = "0";
-
-    SparLog.info("User roles: {}", userAuthenticationHelper.getUserInfo().get().roles());
+    List<Seedlot> seedlots = List.of();
 
     if (!Objects.isNull(pageResult) && pageResult.hasContent()) {
       totalCount = String.valueOf(pageResult.getTotalElements());
+      seedlots = pageResult.getContent();
     }
 
     HttpHeaders responseHeaders = new HttpHeaders();
     responseHeaders.set("X-TOTAL-COUNT", totalCount);
-    return ResponseEntity.ok().headers(responseHeaders).body(pageResult);
+
+    return ResponseEntity.ok().headers(responseHeaders).body(seedlots);
   }
 }
--- backend/src/main/java/ca/bc/gov/backendstartapi/service/TscAdminService.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/service/TscAdminService.java
@@ -1,5 +1,9 @@
 package ca.bc.gov.backendstartapi.service;
 
+import ca.bc.gov.backendstartapi.config.SparLog;
+import ca.bc.gov.backendstartapi.entity.seedlot.Seedlot;
+import ca.bc.gov.backendstartapi.repository.SeedlotRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -7,17 +11,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import ca.bc.gov.backendstartapi.config.SparLog;
-import ca.bc.gov.backendstartapi.entity.seedlot.Seedlot;
-import ca.bc.gov.backendstartapi.repository.SeedlotRepository;
-import lombok.RequiredArgsConstructor;
-
+/** This class holds methods for handling data for the TSC_Admin role. */
 @Service
 @RequiredArgsConstructor
 public class TscAdminService {
 
   private final SeedlotRepository seedlotRepository;
-  
+
   /**
    * Retrieve a paginated list of seedlot for a given user.
    *
@@ -37,8 +37,7 @@ public class TscAdminService {
         PageRequest.of(
             pageNumber, pageSize, Sort.by(Direction.DESC, "AuditInformation_UpdateTimestamp"));
 
-    Page<Seedlot> seedlotPage =
-        seedlotRepository.findAll(sortedPageable);
+    Page<Seedlot> seedlotPage = seedlotRepository.findAll(sortedPageable);
     SparLog.info("{} results and {} pages", seedlotPage.getNumber(), seedlotPage.getTotalPages());
     return seedlotPage;
   }
