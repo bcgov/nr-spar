de2a1bc1ab14666561e3cdbba6f9792d1a6ee19d
Nisarg <nisargvv@gmail.com>
2024-04-09 15:27:23 -0700

    Remove favourite intercepts

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -36,43 +36,6 @@ beforeEach(() => {
   ).as('GET_veg_codes');
 
   cy.intercept(
-    'GET',
-    '**/api/favourite-activities',
-    [
-      {
-        id: 239,
-        userId: 'IDIR@TEST',
-        activity: 'registerAClass',
-        highlighted: false
-      }
-    ]
-  ).as('GET_initial_fav_act_req');
-
-  cy.intercept(
-    'POST',
-    '**/api/favourite-activities',
-    {
-      statusCode: 201
-    }
-  ).as('POST_fav_act_req');
-
-  cy.intercept(
-    'PATCH',
-    '**/api/favourite-activities/**',
-    {
-      statusCode: 200
-    }
-  ).as('PATCH_fav_act_req');
-
-  cy.intercept(
-    'DELETE',
-    '**/api/favourite-activities/**',
-    {
-      statusCode: 200
-    }
-  ).as('DELETE_fav_act_req');
-
-  cy.intercept(
     {
       method: 'GET',
       url: '**/api/seedlots/63001'
