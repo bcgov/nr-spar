318d3a49497deb7dd1cfd91d3ef8ca9605847264
Craig Yu <craig.yu93@gmail.com>
2024-04-26 09:30:52 -0700

    fix: repeated fc calls (#1054)

--- frontend/src/components/OrganizationSelection/index.tsx
+++ frontend/src/components/OrganizationSelection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import {
   FlexGrid, Row, Column,
   ButtonSkeleton, Search, Button,
@@ -48,11 +48,6 @@ const OrganizationSelection = ({ simpleView }: RoleSelectionProps) => {
   });
 
   const qc = useQueryClient();
-
-  // Queries data sometime disappear after awhile, maybe from token refresh, need to refetch them.
-  useEffect(() => {
-    qc.refetchQueries(['role', 'forest-clients']);
-  }, [user]);
 
   const filterClientsByValue = (value: string) => {
     const forestClientsQueriesData = qc.getQueriesData(['role', 'forest-clients']);
