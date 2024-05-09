cc9dc52b11b7d6f2c834309b8db552979eb680dd
Nisarg <nisargvv@gmail.com>
2024-05-09 13:34:32 -0700

    fix: remove comments

--- frontend/cypress/e2e/smoke-test/my-seedlots.cy.ts
+++ frontend/cypress/e2e/smoke-test/my-seedlots.cy.ts
@@ -21,8 +21,6 @@ describe('My seedlots page', () => {
     });
   });
 
-  // TODO separate each test out in its own it()
-  // Test DESC sorting as well
   it('should render my seedlot page heading', () => {
     cy.get('.my-seedlot-title')
       .find('.title-favourite')
@@ -281,59 +279,3 @@ describe('My seedlots page', () => {
     cy.url().should('contains', '/seedlots/register-a-class');
   });
 });
-
-/* Seedlot number sorting
-  Suggested sorting test plan for seedlot_number (lowSeedlotNumber)
-  1. Press the Seedlot number column one time (ASC)
-  2. Get the first row's seedlot number and store it
-  3. Compare the first row's seedlot number to second row's
-  4. Press the Seedlot number column again (DESC)
-  5. Get the first row's seedlot number and store it (name it something else e.g. highSeedlotNumber)
-  6. Compare the first row's seedlot number to second row's
-  7. Compare lowSeedlotNumber and highSeedlotNumber
- */
-
-/* Seedlot species sorting
-  1. Click on the the column one time
-  2. Record the first row's species (lowSpecies)
-  3. Click on the column again
-  4. Record the first row's species (highSpecies)
-  5. Compare the first char of lowSpecies and highSpecies
-  6. lowSpecies.charAt(0) <= highSpecies.charAt(0
-     e.g. cy.expect(lowSpecies.charCodeAt(0)).to.be.at.most(highSpecies.charCodeAt(0))
-*/
-
-/* Seedlot status sorting
-  Similar to species
-*/
-
-/*
-  Updated and Created date sorting
-  Similar to what we have done:
-  Get the two extreme values by clicking on the column 2 times
-  Compare these two extreme values, where the lowest should be lower or equal to the highest
-  in Cypress word, the lowest should be at.most(of the highest)
-*/
-
-// it.only('Date example', () => {
-//   const highest = new Date('May 06, 2024');
-//   const lowest = new Date('May 02, 2024');
-
-//   expect(lowest).to.be.at.most(highest);
-// });
-
-/* Test total num of items
-  The string is in the format of '1–20 of 21 items'
-  get the total number by getting the sub-string between 'of' and 'items'
-  And the total number on the page should be bigger or equal to `NUM_OF_LOOPS * speciesKeys.length`
-*/
-
-/*
-  Test the next page function
-  Since we will have at least 15 rows of seedlot
-  Change the number of rows per page first to 10
-  then click on the next button
-  do some tests
-  click on the back button
-  do some tests
-*/
