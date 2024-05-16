1293163461e013534a6a44bd965a70a73fcf8f21
Craig Yu <craig.yu93@gmail.com>
2024-05-16 09:07:35 -0700

    fix: unused import

--- backend/src/main/java/ca/bc/gov/backendstartapi/exception/GeneticClassNotFoundException.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/exception/GeneticClassNotFoundException.java
@@ -2,7 +2,6 @@ package ca.bc.gov.backendstartapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 /** This class represents an exception when no genetic class is found. */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
