3782477ccffc7a7d655982010b312754864ec108
Craig Yu <craig.yu93@gmail.com>
2024-05-16 16:15:18 -0700

    feat: endpoint and service

---
+++ oracle-api/src/main/java/ca/bc/gov/backendstartapi/repository/SeedPlanZoneCodeRepository.java
@@ -0,0 +1,10 @@
+package ca.bc.gov.backendstartapi.repository;
+
+import ca.bc.gov.backendstartapi.entity.SeedPlanZoneCode;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** This class holds methods for retrieving {@link SeedPlanZoneCode} data from the database. */
+public interface SeedPlanZoneCodeRepository extends JpaRepository<SeedPlanZoneCode, String> {
+  List<SeedPlanZoneCode> findBySpzCodeIn(List<String> spzCodeList);
+}
--- oracle-api/src/main/java/ca/bc/gov/backendstartapi/repository/TestedPtAreaOfUseSpzRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/backendstartapi/repository/TestedPtAreaOfUseSpzRepository.java
@@ -4,10 +4,16 @@ import ca.bc.gov.backendstartapi.entity.TestedPtAreaOfUseSpz;
 import ca.bc.gov.backendstartapi.entity.idclass.TestedPtAreaOfUseSpzId;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /** This class holds methods for retrieving {@link TestedPtAreaOfUseSpz} data from the database. */
 public interface TestedPtAreaOfUseSpzRepository
     extends JpaRepository<TestedPtAreaOfUseSpz, TestedPtAreaOfUseSpzId> {
   List<TestedPtAreaOfUseSpz> findAllByTestedPtAreaOfUse_testedPtAreaOfUseId(
       Integer testedPtAreaOfUseId);
+
+  @Query(
+      value = "SELECT DISTINCT t.seed_plan_zone_code FROM tested_pt_area_of_use_spz t",
+      nativeQuery = true)
+  List<String> findAllDistinctSpz();
 }
