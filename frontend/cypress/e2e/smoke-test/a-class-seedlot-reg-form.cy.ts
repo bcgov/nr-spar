6c54f0bf460c4f4c6d32c11de1806cd7da8eb02e
Nisarg <nisargvv@gmail.com>
2024-05-21 19:25:56 -0700

    feat: add collector agency test

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form.cy.ts
@@ -1,7 +1,11 @@
+import { TYPE_DELAY } from '../../constants';
 import prefix from '../../../src/styles/classPrefix';
+import { SeedlotRegFixtureType } from '../../definitions';
 
-describe('My seedlots page', () => {
+describe('A Class Seedlot Registration form', () => {
   let seedlotNum: string;
+  let species: string;
+  let fixtureData: SeedlotRegFixtureType;
 
   beforeEach(() => {
     // Login
@@ -21,7 +25,18 @@ describe('My seedlots page', () => {
         cy.get(`#seedlot-table-cell-${seedlotNum}-seedlotSpecies`)
           .click();
         cy.url().should('contains', `/seedlots/details/${seedlotNum}`);
+        cy.visit('/seedlots/my-seedlots');
+        return cy.get(`#seedlot-table-cell-${seedlotNum}-seedlotSpecies`);
+      })
+      .then(($data) => {
+        const seedlotSpecies = $data.text();
+        species = (seedlotSpecies.substring(0, seedlotSpecies.indexOf('-') - 1)).toLowerCase();
+        cy.visit(`/seedlots/details/${seedlotNum}`);
       });
+
+    cy.fixture('aclass-seedlot').then((fData) => {
+      fixtureData = fData;
+    });
   });
 
   it('has edit seedlot form button', () => {
@@ -44,6 +59,77 @@ describe('My seedlots page', () => {
 
     cy.get('.seedlot-registration-title')
       .find('.seedlot-form-subtitle')
-      .should('contain.text', 'Seedlot 63011');
+      .should('contain.text', `Seedlot ${seedlotNum}`);
   });
+
+  it('check collector agency section details are correct', () => {
+    cy.get('.detail-section-grid')
+      .find(`.${prefix}--col`)
+      .children('button.section-btn')
+      .click();
+
+    cy.get('#collection-step-default-checkbox')
+      .should('be.checked');
+
+    cy.get('.agency-information-section')
+      .find(`.${prefix}--checkbox-wrapper`)
+      .as('collectorCheckbox')
+      .should('have.text', 'Use applicant agency as collector agency');
+
+    cy.get('#collection-collector-agency')
+      .should('have.value', fixtureData[species].agencyAcronym);
+
+    cy.get('#collection-location-code')
+      .should('have.value', fixtureData[species].agencyNumber);
+
+    // Change inputs
+    cy.get('#collection-step-default-checkbox')
+      .uncheck({ force: true });
+
+    // cy.get('.agency-information-section')
+    //   .find('button.client-search-toggle-btn')
+    //   .click();
+
+    // cy.get(`#${prefix}--modal-body--modal-478`)
+    //   .should('exist');
+
+    // cy.get('#client-search-dropdown')
+    //   .find(`button.${prefix}--list-box__field`)
+    //   .click();
+
+    cy.get('#collection-collector-agency')
+      .type('ggg', { force: true });
+
+    cy.get('#collection-collector-agency-helper-text')
+      .click();
+
+    cy.get('#collection-collector-agency-error-msg')
+      .should('have.text', 'Please enter a valid acronym that identifies the agency');
+
+    cy.get('#collection-collector-agency')
+      .clear()
+      .type(fixtureData.cw.agencyAcronym, { force: true });
+
+    cy.get('#collection-collector-agency-helper-text')
+      .click();
+
+    cy.get('#collection-location-code')
+      .type('96', { force: true, delay: TYPE_DELAY });
+
+    cy.get('#collection-collector-agency-helper-text')
+      .click();
+
+    cy.get('#collection-location-code-error-msg')
+      .should('have.text', 'This location code is not valid for the selected agency, please enter a valid one or change the agency');
+
+    cy.get('#collection-location-code')
+      .clear()
+      .type('02', { force: true });
+
+    cy.get('#collection-collector-agency-helper-text')
+      .click();
+  });
+
+  // it('check collector information section details are coorect', () => {
+  // });
 });
