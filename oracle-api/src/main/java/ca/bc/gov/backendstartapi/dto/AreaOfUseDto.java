8fd979ee443e3e433bd3ba6438ba6491554f43a7
Craig Yu <craig.yu93@gmail.com>
2024-05-24 13:08:02 -0700

    fix: add constructors to area of use dto

--- oracle-api/src/main/java/ca/bc/gov/backendstartapi/dto/AreaOfUseDto.java
+++ oracle-api/src/main/java/ca/bc/gov/backendstartapi/dto/AreaOfUseDto.java
@@ -2,12 +2,16 @@ package ca.bc.gov.backendstartapi.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /** This class represents a JSON response when requesting SPZ + SPU information from a SPU id. */
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(
     description = "Represents a JSON response when requesting SPZ + SPU information from a SPU id.")
 public class AreaOfUseDto {
