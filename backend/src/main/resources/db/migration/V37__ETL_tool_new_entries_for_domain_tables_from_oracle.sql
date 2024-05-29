4b13cdef691c7896822911de7ba589e6e672ed24
Marcio Nemec <marcio.santos@encora.com>
2024-05-29 12:00:55 -0300

    #1180 Including GENETIC CLASS CODE EXTRACT

--- backend/src/main/resources/db/migration/V37__ETL_tool_new_entries_for_domain_tables_from_oracle.sql
+++ backend/src/main/resources/db/migration/V37__ETL_tool_new_entries_for_domain_tables_from_oracle.sql
@@ -46,3 +46,24 @@ select 15 										as execution_id,
        false 									as retry_errors ,
 	   2 										as execution_order
 where not exists (select 1 from spar.etl_execution_map where interface_id = 'SPAR-GAMETIC-METHODOLOGY-ORACLE-TO-POSTGRES-TEST');
+
+-- INCLUDING PROCESS GENETIC_CLASS from Oracle to Postgres in EXECUTION_ID=0  
+insert into spar.etl_execution_map(execution_id, execution_parent_id ,interface_id, source_file,source_name, source_table, source_db_type,
+                                   target_file,target_name, target_table, target_db_type,target_primary_key, 
+								   truncate_before_run ,retry_errors, execution_order)
+select 16 										as execution_id, 
+       0 										as execution_parent_id ,
+       'SPAR-GENETIC-CLASS-ORACLE-TO-POSTGRES-TEST'   as interface_id, 
+       '/SQL/SPAR/ORACLE_GENETIC_CLASS_EXTRACT.sql'   as source_file, 
+       'ORACLE THE'                 			as source_name, 
+       'GENETIC_CLASS_CODE'       				as source_table,
+       'ORACLE'               	    			as source_db_type,
+       '/SQL/SPAR/POSTGRES_GENETIC_CLASS_UPSERT.sql' 	as target_file,
+       'NEW SPAR' 								as target_name, 
+       'spar.genetic_class_list' 			as target_table, 
+       'POSTGRES' 								as target_db_type, 
+       'genetic_class_code' 				as target_primary_key, 
+       false 									as truncate_before_run ,
+       false 									as retry_errors,
+	   3 										as execution_order
+where not exists (select 1 from spar.etl_execution_map where interface_id = 'SPAR-GENETIC-CLASS-ORACLE-TO-POSTGRES-TEST');
