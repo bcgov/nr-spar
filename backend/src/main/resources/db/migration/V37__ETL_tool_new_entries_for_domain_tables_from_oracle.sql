f884a74eca42679e90d548c045bdf35eacc98eaa
Marcio Nemec <marcio.santos@encora.com>
2024-05-29 11:41:43 -0300

    #1180 Including Gametic Methodology Extract from Oracle to postgres

--- backend/src/main/resources/db/migration/V37__ETL_tool_new_entries_for_domain_tables_from_oracle.sql
+++ backend/src/main/resources/db/migration/V37__ETL_tool_new_entries_for_domain_tables_from_oracle.sql
@@ -25,3 +25,24 @@ select 14 										as execution_id,
        false 									as retry_errors ,
 	   1 										as execution_order
 where not exists (select 1 from spar.etl_execution_map where interface_id = 'SPAR-CONE-COLLECTION-ORACLE-TO-POSTGRES-TEST');
+
+-- INCLUDING PROCESS GAMETIC_METHODOLOGY from Oracle to Postgres in EXECUTION_ID=0  
+insert into spar.etl_execution_map(execution_id, execution_parent_id ,interface_id, source_file,source_name, source_table, source_db_type,
+                                   target_file,target_name, target_table, target_db_type,target_primary_key, 
+								   truncate_before_run ,retry_errors, execution_order)
+select 15 										as execution_id, 
+       0 										as execution_parent_id ,
+       'SPAR-GAMETIC-METHODOLOGY-ORACLE-TO-POSTGRES-TEST' 	    as interface_id, 
+       '/SQL/SPAR/ORACLE_GAMETIC_METHODOLOGY_EXTRACT.sql'   as source_file, 
+       'ORACLE THE'                 			as source_name, 
+       'GAMETIC_MTHD_CODE'       				as source_table,
+       'ORACLE'               	    			as source_db_type,
+       '/SQL/SPAR/POSTGRES_GAMETIC_METHODOLOGY__UPSERT.sql' 	as target_file,
+       'NEW SPAR' 								as target_name, 
+       'spar.gametic_methodology_list' 			as target_table, 
+       'POSTGRES' 								as target_db_type, 
+       'gametic_methodology_code' 				as target_primary_key, 
+       false 									as truncate_before_run ,
+       false 									as retry_errors ,
+	   2 										as execution_order
+where not exists (select 1 from spar.etl_execution_map where interface_id = 'SPAR-GAMETIC-METHODOLOGY-ORACLE-TO-POSTGRES-TEST');
