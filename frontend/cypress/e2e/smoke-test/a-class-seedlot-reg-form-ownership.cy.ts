f8902f6982b65ebe3e490e59f9f99b5c30c8017e
Nisarg <nisargvv@gmail.com>
2024-05-31 13:42:49 -0700

    feat: fixture data and beforeEach

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-ownership.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-ownership.cy.ts
@@ -1,4 +1,50 @@
+import { SeedlotRegFixtureType } from '../../definitions';
+import prefix from '../../../src/styles/classPrefix';
+
 describe('A Class Seedlot Registration form, Ownership', () => {
+  let regFormData: {
+    title: string;
+    subtitle: string;
+    agencyTitle: string;
+    agencySubtitle: string;
+    informationTitle: string;
+    informationSubtitle: string;
+    checkboxText: string;
+    acronymErrorMsg: string;
+    locationErrorMsg: string;
+    invalidDateErrorMsg: string;
+    numOfContainerErrorMsg: string;
+    volOfConesErrorMsg: string;
+  };
+
+  let seedlotNum: string;
+  const speciesKey = 'pli';
+  let seedlotData: SeedlotRegFixtureType;
+  let testAcronym: string;
+  let testPopupAcronym: string;
+
+  beforeEach(() => {
+    // Login
+    cy.login();
+
+    cy.fixture('a-class-reg-form-ownership').then((fData) => {
+      regFormData = fData;
+    });
+
+    cy.fixture('aclass-seedlot').then((fData) => {
+      seedlotData = fData;
+      cy.task('getData', fData[speciesKey].species).then((sNumber) => {
+        seedlotNum = sNumber as string;
+        cy.visit(`/seedlots/a-class-registration/${seedlotNum}`);
+        cy.url().should('contains', `/seedlots/a-class-registration/${seedlotNum}`);
+      });
+      testAcronym = seedlotData.dr.agencyAcronym;
+      testPopupAcronym = seedlotData.cw.agencyAcronym;
+      cy.get(`button.${prefix}--progress-step-button[title="Ownership"]`)
+        .click();
+    });
+  });
   it('check title and subtitles', () => {
+
   });
 });
