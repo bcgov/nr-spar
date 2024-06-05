631d8c88918937cae50faa03efba8d09e63f9015
Craig Yu <craig.yu93@gmail.com>
2024-06-05 14:08:55 -0700

    fix: restore fc client api (#1202)

--- frontend/src/api-service/forestClientsAPI.ts
+++ frontend/src/api-service/forestClientsAPI.ts
@@ -1,58 +1,17 @@
-import { AxiosError } from 'axios';
 import ApiConfig from './ApiConfig';
 import api from './api';
 import { ForestClientType } from '../types/ForestClientTypes/ForestClientType';
-import { MockForestClients, UNKOWN_FC } from './fixtures/MockForestClients';
 import { ForestClientSearchType } from '../types/ForestClientTypes/ForestClientSearchType';
 import { ClientSearchOptions } from '../components/ApplicantAgencyFields/ClientSearchModal/definitions';
 
-// eslint-disable-next-line arrow-body-style, @typescript-eslint/no-unused-vars
 export const getForestClientLocation = (clientNumber: string, locationCode: string) => {
-  // TODO: restore this once forest client is back up
-  // const url = `${ApiConfig.forestClient}/${clientNumber}/location/${locationCode}`;
-  // return api.get(url).then((res) => res.data);
-
-  // default mocked false number
-  if (locationCode === '99') {
-    return Promise.reject(new AxiosError("don't worry it's fine"));
-  }
-
-  return Promise.resolve();
+  const url = `${ApiConfig.forestClient}/${clientNumber}/location/${locationCode}`;
+  return api.get(url).then((res) => res.data);
 };
 
-// eslint-disable-next-line arrow-body-style
 export const getForestClientByNumberOrAcronym = (numberOrAcronym: string) => {
-  // TODO: restore this once forest client is back up
-  // const url = `${ApiConfig.forestClient}/${clientNumber}`;
-  // return api.get(url).then((res): ForestClientType => res.data);
-
-  return Promise.resolve(
-    MockForestClients.find(
-      (fc) => fc.clientNumber === numberOrAcronym || fc.acronym === numberOrAcronym
-    ) ?? UNKOWN_FC
-  );
-};
-
-export const getAllAgencies = (): string[] => {
-  const options: string[] = [];
-  MockForestClients.sort(
-    (a: ForestClientType, b: ForestClientType) => (a.clientName < b.clientName ? -1 : 1)
-  );
-  MockForestClients.forEach((agency: ForestClientType) => {
-    let clientName = agency.clientName
-      .toLowerCase()
-      .split(' ')
-      .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
-    if (clientName.indexOf('-') > -1) {
-      clientName = clientName
-        .split('-')
-        .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join('-');
-    }
-    options.push(`${agency.clientNumber} - ${clientName} - ${agency.acronym}`);
-  });
-  return options;
+  const url = `${ApiConfig.forestClient}/${numberOrAcronym}`;
+  return api.get(url).then((res): ForestClientType => res.data);
 };
 
 export const searchForestClients = (
