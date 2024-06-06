64c3893a839f79b4a7c67c7a230589b626a413aa
mgaseta <matheus.gaseta@encora.com>
2024-06-06 11:29:20 -0300

    fix: updating isAdmin token when refreshing the page

--- frontend/src/contexts/AuthProvider.tsx
+++ frontend/src/contexts/AuthProvider.tsx
@@ -194,6 +194,7 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }: Pro
         if (restoredClientName) {
           foundClient.clientName = restoredClientName;
         }
+        setIsTscAdmin(foundClient.roles.includes(TSC_ADMIN_ROLE));
         setSelectedClientRoles(foundClient);
       }
     }
