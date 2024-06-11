f181d88c23f0aa97bf58d2164550b4e90275beee
Nisarg <nisargvv@gmail.com>
2024-06-11 09:57:57 -0700

    feat: add title and subtitle test

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-orchard.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-orchard.cy.ts
@@ -1,4 +1,68 @@
+import { SeedlotRegFixtureType } from '../../definitions';
+
 describe('A Class Seedlot Registration form, Orchard', () => {
+  let regFormData: {
+    orchard: {
+      title: string;
+      subtitle: string;
+      gameteTitle: string;
+      gameteSubtitle: string;
+      pollenTitle: string;
+      pollenSubtitle: string;
+    }
+  };
+
+  let seedlotNum: string;
+  const speciesKey = 'pli';
+  let seedlotData: SeedlotRegFixtureType;
+
+  beforeEach(() => {
+    // Login
+    cy.login();
+    cy.fixture('aclass-reg-form').then((fData) => {
+      regFormData = fData;
+    });
+
+    cy.fixture('aclass-seedlot').then((fData) => {
+      seedlotData = fData;
+      cy.task('getData', fData[speciesKey].species).then((sNumber) => {
+        seedlotNum = sNumber as string;
+        const url = `/seedlots/a-class-registration/${seedlotNum}/?step=4`;
+        cy.visit(url);
+        cy.url().should('contains', url);
+      });
+    });
+  });
+
   it('Page title and subtitles', () => {
+    cy.get('.seedlot-orchard-title-row')
+      .find('h2')
+      .eq(0)
+      .should('have.text', regFormData.orchard.title);
+
+    cy.get('.seedlot-orchard-title-row')
+      .find('.subtitle-section')
+      .eq(0)
+      .should('have.text', regFormData.orchard.subtitle);
+
+    cy.get('.seedlot-orchard-title-row')
+      .find('h2')
+      .eq(1)
+      .should('have.text', regFormData.orchard.gameteTitle);
+
+    cy.get('.seedlot-orchard-title-row')
+      .find('.subtitle-section')
+      .eq(1)
+      .should('have.text', regFormData.orchard.gameteSubtitle);
+
+    cy.get('.seedlot-orchard-title-row')
+      .find('h2')
+      .eq(2)
+      .should('have.text', regFormData.orchard.pollenTitle);
+
+    cy.get('.seedlot-orchard-title-row')
+      .find('.subtitle-section')
+      .eq(2)
+      .should('have.text', regFormData.orchard.pollenSubtitle);
   });
 });
