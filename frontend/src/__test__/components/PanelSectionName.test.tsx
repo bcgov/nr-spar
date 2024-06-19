cd794e13e19f7e781f9568c623298488959f4622
Derek Roberts <derek.roberts@gmail.com>
2024-06-18 19:06:30 -0700

    Remove @testing-library/jest-dom

--- frontend/src/__test__/components/PanelSectionName.test.tsx
+++ frontend/src/__test__/components/PanelSectionName.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import PanelSectionName from '../../components/PanelSectionName/index';
-import '@testing-library/jest-dom';
 
 describe('Test the panel section name component', () => {
   it('should render correctly', () => {
--- frontend/src/__test__/views/CreateAClass.test.tsx
+++ frontend/src/__test__/views/CreateAClass.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react';
 import CreateAClass from '../../views/Seedlot/CreateAClass/index';
-import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import makeServer from '../../mock-server/server';
