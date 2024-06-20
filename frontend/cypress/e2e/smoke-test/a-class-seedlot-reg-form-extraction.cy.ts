2ade6d6b9fd51a9951b671f15743190ae2fbafc4
Nisarg <nisargvv@gmail.com>
2024-06-20 10:26:03 -0700

    feat: add title and subtitle test

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-extraction.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-extraction.cy.ts
@@ -34,6 +34,20 @@ describe('A Class Seedlot Registration form, Extraction and Storage', () => {
   });
 
   it('Page title and subtitles', () => {
+    cy.get('.extraction-information-title')
+      .find('h2')
+      .should('have.text', regFormData.extraction.extrationTitle);
 
+    cy.get('.extraction-information-title')
+      .find('.subtitle-section')
+      .should('have.text', regFormData.extraction.extrationSubtitle);
+
+    cy.get('.temporary-seed-storage-title')
+      .find('h2')
+      .should('have.text', regFormData.extraction.storageTitle);
+
+    cy.get('.temporary-seed-storage-title')
+      .find('.subtitle-section')
+      .should('have.text', regFormData.extraction.storageSubtitle);
   });
 });
