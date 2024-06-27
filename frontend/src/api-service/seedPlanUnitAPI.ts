5f42632bd514b0e20fa381196cfd968efd39710a
Craig Yu <craig.yu93@gmail.com>
2024-06-27 11:20:10 -0700

    fix: remove unused api

--- frontend/src/api-service/seedPlanUnitAPI.ts
+++
@@ -1,7 +0,0 @@
-import ApiConfig from './ApiConfig';
-import api from './api';
-
-export const getSpuById = (spuId: string) => {
-  const url = `${ApiConfig.seedPlanUnit}/${spuId}`;
-  return api.get(url).then((res) => res.data);
-};
