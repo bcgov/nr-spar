43d678ff7e5e4ca3f3c373a9a3e0d6f14a7146d8
Ricardo Campos <ricardo.campos@encora.com>
2024-07-01 18:01:40 -0300

    feat: fix seedlot audit table and create a script to help fix it
    not exactly on the issue #1254

---
+++ tools/pg-check-seedlot-audit.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+CONTAINER_ID="efa380b99500"
+DB_USER="postgres"
+DB_NAME="spar"
+
+__SEEDLOT_QUERY="
+SELECT c.column_name
+FROM information_schema.columns c
+WHERE c.table_schema = '$DB_NAME'
+  AND c.table_name = 'seedlot'
+"
+
+__SEEDLOT_AUDIT_QUERY="
+SELECT c.column_name
+FROM information_schema.columns c
+WHERE c.table_schema = '$DB_NAME'
+  AND table_name = 'seedlot_audit'
+  AND c.column_name NOT IN (
+    'audit_date',
+    'audit_revision_version',
+    'db_user',
+    'seedlot_audit_id',
+    'spar_audit_code'
+  )
+"
+
+# Get Seedlot columns
+docker exec $CONTAINER_ID \
+  psql -q -t -U $DB_USER -d $DB_NAME \
+  -c "$__SEEDLOT_QUERY" > /tmp/seedlot_check.txt
+
+# Get Seedlot Audit columns
+docker exec $CONTAINER_ID \
+  psql -q -t -U $DB_USER -d $DB_NAME \
+  -c "$__SEEDLOT_AUDIT_QUERY" > /tmp/seedlot_audit_check.txt
+
+diff /tmp/seedlot_check.txt /tmp/seedlot_audit_check.txt
