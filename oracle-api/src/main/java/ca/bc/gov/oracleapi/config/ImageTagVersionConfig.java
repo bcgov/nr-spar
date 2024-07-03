b2090b7a55e74caf0edc9df176d3cb5d5436eb6d
Ricardo Campos <ricardo.campos@encora.com>
2024-07-03 11:57:59 -0300

    feat: show image tag version on actuator health page

---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/config/ImageTagVersionConfig.java
@@ -0,0 +1,19 @@
+package ca.bc.gov.oracleapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/** This class contains a single component config with image tag version. */
+@Component
+public class ImageTagVersionConfig implements HealthIndicator {
+  
+  @Value("${nr-spar-oracle-api.version}")
+  private String nrSparOracleApiVersion;
+
+  @Override
+  public Health health() {
+    return Health.up().withDetail("ImageTag", nrSparOracleApiVersion).build();
+  }
+}
