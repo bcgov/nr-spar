20708fbffe2d803b2b6f6bab13d1a416f0b217fd
Craig Yu <craig.yu93@gmail.com>
2024-07-04 17:27:05 -0700

    fix: replace toString() with String() (#1341)

--- frontend/src/contexts/AuthProvider.tsx
+++ frontend/src/contexts/AuthProvider.tsx
@@ -219,7 +219,7 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }: Pro
     const appEnv = env.VITE_ZONE ?? 'DEV';
 
     Auth.federatedSignIn({
-      customProvider: `${(appEnv).toLocaleUpperCase()}-${signInProvider.toString()}`
+      customProvider: `${(appEnv).toLocaleUpperCase()}-${String(signInProvider)}`
     });
   };
 
