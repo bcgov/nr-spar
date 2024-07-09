06211481e374257927cc043d1e969cac5cc2db5c
Paulo Gomes da Cruz Junior <paulushc@gmail.com>
2024-07-09 15:58:20 -0700

    feat: adding Prometheus support

--- backend/src/main/java/ca/bc/gov/backendstartapi/BackendStartApiApplication.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/BackendStartApiApplication.java
@@ -2,9 +2,11 @@ package ca.bc.gov.backendstartapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /** Starting point of the service app. */
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class BackendStartApiApplication {
 
   public static void main(String[] args) {
