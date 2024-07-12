8aa7b43bac7653228c2edba07e8475b8b17ff8ca
Ricardo Campos <ricardo.campos@encora.com>
2024-07-12 17:58:01 -0300

    feat: creat spar progress bar with custom icons

--- frontend/src/components/SeedlotRegistrationProgress/index.tsx
+++ frontend/src/components/SeedlotRegistrationProgress/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import {
-  ProgressIndicator,
-  ProgressStep
-} from '@carbon/react';
-
+import { SparProgressIndicator, SparProgressStep } from './SparProgressIndicator';
 import { ProgressIndicatorConfig } from '../../views/Seedlot/ContextContainerClassA/definitions';
 import { MEDIUM_SCREEN_WIDTH } from '../../shared-constants/shared-constants';
 import useWindowSize from '../../hooks/UseWindowSize';
@@ -13,7 +9,7 @@ import './styles.scss';
 
 interface SeedlotRegistrationProgressProps {
   progressStatus: ProgressIndicatorConfig;
-  interactFunction?: Function;
+  interactFunction?: (data: number) => void;
 }
 
 const SeedlotRegistrationProgress = ({
@@ -22,57 +18,57 @@ const SeedlotRegistrationProgress = ({
 }: SeedlotRegistrationProgressProps) => {
   const widowSize = useWindowSize();
   return (
-    <ProgressIndicator
+    <SparProgressIndicator
       // Needs to feed it a -1 value otherwise step 1 will stuck at current
       className="spar-seedlot-reg-progress-bar"
       currentIndex={-1}
       spaceEqually
-      onChange={interactFunction ?? null}
+      onChange={interactFunction ?? undefined}
       vertical={widowSize.innerWidth < MEDIUM_SCREEN_WIDTH}
     >
-      <ProgressStep
+      <SparProgressStep
         label="Collection"
         secondaryLabel="Step 1"
         complete={progressStatus.collection.isComplete}
         current={progressStatus.collection.isCurrent}
         invalid={progressStatus.collection.isInvalid}
       />
-      <ProgressStep
+      <SparProgressStep
         label="Ownership"
         secondaryLabel="Step 2"
         complete={progressStatus.ownership.isComplete}
         current={progressStatus.ownership.isCurrent}
         invalid={progressStatus.ownership.isInvalid}
       />
-      <ProgressStep
+      <SparProgressStep
         label="Interim storage"
         secondaryLabel="Step 3"
         complete={progressStatus.interim.isComplete}
         current={progressStatus.interim.isCurrent}
         invalid={progressStatus.interim.isInvalid}
       />
-      <ProgressStep
+      <SparProgressStep
         label="Orchard"
         secondaryLabel="Step 4"
         complete={progressStatus.orchard.isComplete}
         current={progressStatus.orchard.isCurrent}
         invalid={progressStatus.orchard.isInvalid}
       />
-      <ProgressStep
+      <SparProgressStep
         label="Parent tree and SMP"
         secondaryLabel="Step 5"
         complete={progressStatus.parent.isComplete}
         current={progressStatus.parent.isCurrent}
         invalid={progressStatus.parent.isInvalid}
       />
-      <ProgressStep
+      <SparProgressStep
         label="Extraction and storage"
         secondaryLabel="Step 6"
         complete={progressStatus.extraction.isComplete}
         current={progressStatus.extraction.isCurrent}
         invalid={progressStatus.extraction.isInvalid}
       />
-    </ProgressIndicator>
+    </SparProgressIndicator>
   );
 };
 
