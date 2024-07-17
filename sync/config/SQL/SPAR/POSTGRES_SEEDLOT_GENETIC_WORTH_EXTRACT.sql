a736c50d3e59d6a62097538097d751c398985edb
ronrobb <ron.robb@cgi.com>
2024-07-17 11:35:26 -0700

    userid changes and seedlot_status_code rule implementation

--- sync/config/SQL/SPAR/POSTGRES_SEEDLOT_GENETIC_WORTH_EXTRACT.sql
+++ sync/config/SQL/SPAR/POSTGRES_SEEDLOT_GENETIC_WORTH_EXTRACT.sql
@@ -2,9 +2,9 @@ SELECT
   seedlot_number
 , genetic_worth_code
 , genetic_quality_value                genetic_worth_rtng                   
-, entry_userid
+, REPLACE(entry_userid,'\', '@') as entry_userid
 , entry_timestamp
-, update_userid
+, REPLACE(update_userid,'\', '@') as update_userid
 , update_timestamp
 , revision_count
  FROM spar.seedlot_genetic_worth sgw
