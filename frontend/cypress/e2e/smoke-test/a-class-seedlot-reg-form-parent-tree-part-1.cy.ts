8e510c9e47e48b09fa0816ee130014324a923ca3
Nisarg <nisargvv@gmail.com>
2024-07-25 15:17:59 -0700

    feat: add test of title and invalid entries error msg

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-1.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-1.cy.ts
@@ -5,17 +5,13 @@ import { SeedlotRegFixtureType } from '../../definitions';
 describe('A Class Seedlot Registration form, Parent Tree and SMP part-1(Cone and Pollen count)', () => {
   let regFormData: {
     parentTree: {
-      parentTreeTitle: string;
-      parentTreeSubtitle: string;
+      title: string;
+      subtitle: string;
       coneTitle: string;
       coneSubtitle: string;
-      smpSuccessTitle: string;
-      smpSuccessSubtitle: string;
-      calculationSMPTitle: string;
-      calculationSMPSubtitle: string;
-      smpSuccessCheckboxText: string;
       coneErrorMsg: string;
       pollenErrorMsg: string;
+      conePollenErrorMsg: string;
     }
   };
 
@@ -42,5 +38,102 @@ describe('A Class Seedlot Registration form, Parent Tree and SMP part-1(Cone and
   });
 
   it('Page title and subtitles', () => {
+    cy.get('.title-row')
+      .find('h2')
+      .should('have.text', regFormData.parentTree.title);
+
+    cy.get('.subtitle-section')
+      .should('have.text', regFormData.parentTree.subtitle);
+
+    cy.get('.parent-tree-step-table-container')
+      .find('h4')
+      .should('have.text', regFormData.parentTree.coneTitle);
+
+    cy.get('.parent-tree-step-table-container')
+      .find(`p.${prefix}--data-table-header__description`)
+      .should('have.text', regFormData.parentTree.coneSubtitle);
+  });
+
+  it('Cone and pollen count table entries', () => {
+    // Check error message for negative Cone count
+    cy.get('#212-coneCount-value-input')
+      .type('-1')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .find(`.${prefix}--actionable-notification__title`)
+      .as('errorDialog')
+      .should('have.text', regFormData.parentTree.coneErrorMsg);
+
+    cy.get('.subtitle-section')
+      .should('have.text', regFormData.parentTree.subtitle);
+
+    // Check error message for Cone count > 10000000000
+    cy.get('#212-coneCount-value-input')
+      .clear()
+      .type('10000000001')
+      .blur();
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.coneErrorMsg);
+
+    // Check error message for Cone count value > 10 decimal places
+    cy.get('#212-coneCount-value-input')
+      .clear()
+      .type('0.00000000001')
+      .blur();
+  
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.coneErrorMsg);
+
+    // Check no error message for positive Cone count
+    cy.get('#212-coneCount-value-input')
+      .clear()
+      .type('1')
+      .blur();
+  
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('not.exist');
+
+    // Check error message for negative Pollen count
+    cy.get('#212-pollenCount-value-input')
+      .type('-1')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.pollenErrorMsg);
+
+    // Check error message for Pollen count > 10000000000
+    cy.get('#212-pollenCount-value-input')
+      .clear()
+      .type('10000000001')
+      .blur();
+  
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.coneErrorMsg);
+  
+    // Check error message for Pollen count value > 10 decimal places
+    cy.get('#212-pollenCount-value-input')
+      .clear()
+      .type('0.00000000001')
+      .blur();
+  
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.coneErrorMsg);
+
+    // Check error message for negative Cone count and negative Pollen count
+    cy.get('#212-coneCount-value-input')
+      .clear()
+      .type('-1')
+      .blur();
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.conePollenErrorMsg);
   });
 });
