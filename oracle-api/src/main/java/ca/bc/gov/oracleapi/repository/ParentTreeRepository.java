4012dca363e1252fa75ced6b1260a3af7aeef147
Craig Yu <craig.yu93@gmail.com>
2024-07-30 13:56:17 -0700

    fix: pt query

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/ParentTreeRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/ParentTreeRepository.java
@@ -15,10 +15,11 @@ public interface ParentTreeRepository extends JpaRepository<ParentTreeEntity, Lo
   @Query(
       value =
           """
-            SELECT DISTINCT PT.PARENT_TREE_ID AS \"parentTreeId\",
+            SELECT PT.PARENT_TREE_ID AS \"parentTreeId\",
               PT.PARENT_TREE_NUMBER AS \"parentTreeNumber\",
-              O.ORCHARD_ID AS \"orchardId\", Q.SEED_PLAN_UNIT_ID AS \"spu\",
-              PT.TESTED_IND AS \"tested\"
+              O.ORCHARD_ID AS \"orchardId\", PTSPU.SEED_PLAN_UNIT_ID AS \"spu\",
+              PT.TESTED_IND AS \"tested\", Q.GENETIC_TYPE_CODE, Q.GENETIC_WORTH_CODE,
+              Q.GENETIC_QUALITY_VALUE
             FROM PARENT_TREE PT
             JOIN PARENT_TREE_ORCHARD PTO
               ON PTO.PARENT_TREE_ID = PT.PARENT_TREE_ID
@@ -26,7 +27,11 @@ public interface ParentTreeRepository extends JpaRepository<ParentTreeEntity, Lo
               ON O.ORCHARD_ID = PTO.ORCHARD_ID
             LEFT JOIN PARENT_TREE_GENETIC_QUALITY Q
               ON PT.PARENT_TREE_ID = Q.PARENT_TREE_ID
+              AND Q.GENETIC_WORTH_CALC_IND = 'Y'
+            LEFT JOIN PARENT_TREE_SEED_PLAN_UNIT PTSPU
+              ON PTSPU.PARENT_TREE_ID = PT.PARENT_TREE_ID
             WHERE PT.VEGETATION_CODE = ?1
+              AND PT.ACTIVE_IND = 'Y'
               AND PT.parent_tree_reg_status_code = 'APP'
           """,
       nativeQuery = true)
