f5ac5f8707b933d26c0b43210f8190b98b2ad51f
Ricardo Campos <ricardo.campos@encora.com>
2024-08-02 16:29:48 -0300

    feat: logical lock for the seedlot (#1455)

--- backend/src/main/java/ca/bc/gov/backendstartapi/service/SeedlotOrchardService.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/service/SeedlotOrchardService.java
@@ -7,6 +7,7 @@ import ca.bc.gov.backendstartapi.entity.seedlot.SeedlotOrchard;
 import ca.bc.gov.backendstartapi.exception.SeedlotConflictDataException;
 import ca.bc.gov.backendstartapi.repository.SeedlotOrchardRepository;
 import ca.bc.gov.backendstartapi.security.LoggedUserService;
+import ca.bc.gov.backendstartapi.util.ValueUtil;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -38,7 +39,10 @@ public class SeedlotOrchardService {
     seedlot.setProducedWithBiotechnologicalProcesses(formStep4.biotechProcessesInd());
     seedlot.setPollenContaminationPresentInOrchard(formStep4.pollenContaminationInd());
     seedlot.setPollenContaminationPercentage(formStep4.pollenContaminationPct());
-    seedlot.setPollenContaminantBreedingValue(formStep4.contaminantPollenBv());
+    if (!ValueUtil.isValueEqual(
+        formStep4.contaminantPollenBv(), seedlot.getPollenContaminantBreedingValue())) {
+      seedlot.setPollenContaminantBreedingValue(formStep4.contaminantPollenBv());
+    }
     seedlot.setPollenContaminationMethodCode(formStep4.pollenContaminationMthdCode());
 
     SparLog.info(
@@ -54,6 +58,12 @@ public class SeedlotOrchardService {
     }
     List<SeedlotOrchard> seedlotOrchards = getAllSeedlotOrchardBySeedlotNumber(seedlot.getId());
 
+    boolean allEqual = areExistingEqualsNewOnes(seedlotOrchards, formStep4);
+    if (allEqual) {
+      SparLog.info("Do not need to touch seedlot orchards, they are the same");
+      return;
+    }
+
     if (!seedlotOrchards.isEmpty() && canDelete) {
       SparLog.info(
           "Deleting {} previous records on the SeedlotOrchard table for seedlot number {}",
@@ -61,12 +71,38 @@ public class SeedlotOrchardService {
           seedlot.getId());
 
       seedlotOrchardRepository.deleteAllBySeedlot_id(seedlot.getId());
+      seedlotOrchardRepository.flush();
     } else if (!seedlotOrchards.isEmpty() && !canDelete) {
       SparLog.info("Update seedlot {} orchard data failed due to conflict.", seedlot.getId());
       throw new SeedlotConflictDataException(seedlot.getId());
     }
 
     saveSeedlotOrchards(seedlot, formStep4.primaryOrchardId(), formStep4.secondaryOrchardId());
+  }
+
+  private boolean areExistingEqualsNewOnes(
+      List<SeedlotOrchard> seedlotOrchards, SeedlotFormOrchardDto formStep4) {
+    // Primary
+    Optional<SeedlotOrchard> primaryOpt =
+        seedlotOrchards.stream().filter(SeedlotOrchard::getIsPrimary).findFirst();
+
+    if (primaryOpt.isEmpty()) {
+      return false;
+    }
+
+    boolean primaryEqual = ValueUtil.isValueEqual(primaryOpt.get(), formStep4.primaryOrchardId());
+
+    // Secondary
+    Optional<SeedlotOrchard> secondaryOpt =
+        seedlotOrchards.stream().filter(so -> !so.getIsPrimary()).findFirst();
+
+    if (secondaryOpt.isEmpty()) {
+      return primaryEqual;
+    }
+
+    boolean secondaryEqual =
+        ValueUtil.isValueEqual(secondaryOpt.get(), formStep4.secondaryOrchardId());
+    return secondaryEqual && primaryEqual;
   }
 
   private void saveSeedlotOrchards(
