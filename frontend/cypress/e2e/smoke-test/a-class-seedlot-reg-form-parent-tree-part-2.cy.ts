3747491e80828906f0b8c2e291507f22a4872cb5
Nisarg <nisargvv@gmail.com>
2024-08-15 16:50:21 -0700

    feat: update checkbox test

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-2.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-2.cy.ts
@@ -66,10 +66,132 @@ describe('A Class Seedlot Registration form, Parent Tree and SMP part-2(SMP succ
     cy.get('.smp-default-input-row')
       .should('be.visible');
 
+    // Check error msg for negative 'SMP success' value
     cy.get('#default-smp-success-input')
-      .type('5');
+      .type('-1')
+      .blur();
 
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .find(`.${prefix}--actionable-notification__title`)
+      .as('errorDialog')
+      .should('have.text', regFormData.parentTree.smpSuccessErrorMsg);
+
+    // Check no error message for positive 'SMP success' value
+    cy.get('#default-smp-success-input')
+      .type('5')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('not.exist');
+
+    // Check error msg for decimal 'SMP success' value
+    cy.get('#default-smp-success-input')
+      .type('0.05')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.smpSuccessErrorMsg);
+
+    // Check error msg for >25 'SMP success' value
+    cy.get('#default-smp-success-input')
+      .type('26')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.smpSuccessErrorMsg);
+
+    // Check error msg for negative 'pollen contaminant' value
     cy.get('#default-pollen-contam-input')
-      .type('5');
+      .type('-1')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.nonOrchardErrorMsg);
+
+    // Check no error message for positive 'pollen contaminant' value
+    cy.get('#default-pollen-contam-input')
+      .type('5')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('not.exist');
+
+    // Check error msg for decimal 'pollen contaminant' value
+    cy.get('#default-pollen-contam-input')
+      .type('0.05')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.nonOrchardErrorMsg);
+
+    // Check error msg for >100 'pollen contaminant' value
+    cy.get('#default-pollen-contam-input')
+      .type('101')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('be.visible');
+
+    cy.get('@errorDialog')
+      .should('have.text', regFormData.parentTree.nonOrchardErrorMsg);
+
+    // Enter values for all 'SMP success on parent (%)' cells
+    cy.get('#default-smp-success-input')
+      .type('5')
+      .blur();
+
+    // Enter values for all 'Non-orchard pollen contam. (%)' cells
+    cy.get('#default-pollen-contam-input')
+      .type('2')
+      .blur();
+
+    cy.get(`.${prefix}--actionable-notification--error`)
+      .should('not.exist');
+
+    // Check values for top 5 rows
+    cy.get('#212-smpSuccessPerc-value-input')
+      .should('have.value', '5');
+
+    cy.get('#212-nonOrchardPollenContam-value-input')
+      .should('have.value', '2');
+
+    cy.get('#219-smpSuccessPerc-value-input')
+      .should('have.value', '5');
+
+    cy.get('#219-nonOrchardPollenContam-value-input')
+      .should('have.value', '2');
+
+    cy.get('#222-smpSuccessPerc-value-input')
+      .should('have.value', '5');
+
+    cy.get('#222-nonOrchardPollenContam-value-input')
+      .should('have.value', '2');
+
+    cy.get('#223-smpSuccessPerc-value-input')
+      .should('have.value', '5');
+
+    cy.get('#223-nonOrchardPollenContam-value-input')
+      .should('have.value', '2');
+
+    cy.get('#224-smpSuccessPerc-value-input')
+      .should('have.value', '5');
+
+    cy.get('#224-nonOrchardPollenContam-value-input')
+      .should('have.value', '2');
   });
 });
