a319abcc6323391d677d9b495121c557c0440011
Nisarg <nisargvv@gmail.com>
2024-09-04 18:53:55 -0700

    feat: add show/hide test

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts
@@ -39,4 +39,142 @@ describe('A Class Seedlot Registration form, Parent Tree and SMP part-3(Calculat
     cy.get(`.${prefix}--data-table-header__description`)
       .should('have.text', regFormData.parentTree.calculationSubtitle);
   });
+
+  it('Check \'Show/hide columns\' button functionality', () => {
+    // Wait for the table to load
+    cy.get('#parentTreeNumber');
+
+    // Click 'Dothistroma needle blight (DFS)' checkbox
+    cy.get(`.${prefix}--toolbar-content > span`)
+      .eq(1)
+      .find('button')
+      .as('clickShowHideBtn')
+      .click({force: true});
+
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Dothistroma needle blight (DFS)')
+      .click();
+
+    cy.get('.parent-tree-step-table-container')
+      .find('h4')
+      .as('closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#dfs')
+      .should('exist');
+
+    // Click 'Comandra blister rust (DSC)' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Comandra blister rust (DSC)')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#dsc')
+      .should('exist');
+
+    // Click 'Western gall rust (DSG)' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Western gall rust (DSG)')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#dsg')
+      .should('exist');
+
+    // Click 'Volume growth (GVO)' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Volume growth (GVO)')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#gvo')
+      .should('exist');
+
+    // Click 'Weighted DFS' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+  
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Weighted DFS')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#w_dfs')
+      .should('exist');
+
+    // Click 'Weighted DSC' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+  
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Weighted DSC')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#w_dsc')
+      .should('exist');
+
+    // Click 'Weighted DSG' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+  
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Weighted DSG')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#w_dsg')
+      .should('exist');
+
+    // Click 'Weighted GVO' checkbox
+    cy.get('@clickShowHideBtn')
+      .click({force: true});
+  
+    cy.get('ul.parent-tree-table-toggle-menu')
+      .find('li')
+      .contains('Weighted GVO')
+      .click();
+
+    cy.get('@closeShowHideDropdown')
+      .click();
+
+    cy.get('thead.table-header')
+      .find('#w_gvo')
+      .should('exist');
+  });
 });
