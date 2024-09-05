dfb35df3841163b7c3541bd55222726436a5fd3b
Nisarg <nisargvv@gmail.com>
2024-09-04 19:00:38 -0700

    feat: add more options test

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts
@@ -177,4 +177,93 @@ describe('A Class Seedlot Registration form, Parent Tree and SMP part-3(Calculat
       .find('#w_gvo')
       .should('exist');
   });
+
+  it('Check \'More Options\' button functionality', () => {
+    // Wait for the table to load
+    cy.get('#parentTreeNumber');
+
+    // Check Download file option
+    cy.get(`.${prefix}--toolbar-content > span`)
+      .eq(2)
+      .find('button')
+      .as('clickMoreOptionsBtn')
+      .click();
+
+    cy.get('ul.parent-tree-table-option-menu')
+      .find('li')
+      .contains('Download table template')
+      .click();
+
+    cy.readFile(`${Cypress.config('downloadsFolder')}/Seedlot_composition_template.csv`);
+
+    // Click 'Clean table data' option
+    cy.get('@clickMoreOptionsBtn')
+      .click();
+
+    cy.get('ul.parent-tree-table-option-menu')
+      .find('li')
+      .contains('Clean table data')
+      .as('clickCleanTableBtn')
+      .click();
+
+    cy.get(`.${prefix}--modal-container[aria-label="Clean table data"]`)
+      .should('be.visible');
+
+    // Check Cancel button of 'Clean table data' dialog box
+    cy.get(`.${prefix}--modal-container[aria-label="Clean table data"]`)
+      .find('button')
+      .contains('Cancel')
+      .click();
+
+    cy.get(`.${prefix}--modal-container[aria-label="Clean table data"]`)
+      .should('not.be.visible');
+
+    // Check 'X' button of 'Clean table data' dialog box
+    cy.get('@clickMoreOptionsBtn')
+      .click();
+
+    cy.get('@clickCleanTableBtn')
+      .click();
+
+    cy.get(`.${prefix}--modal-container[aria-label="Clean table data"]`)
+      .find('button')
+      .contains('Cancel')
+      .click();
+
+    cy.get(`.${prefix}--modal-container[aria-label="Clean table data"]`)
+      .should('not.be.visible');
+
+    // Check 'Clean table data' button of 'Clean table data' dialog box
+    cy.get('@clickMoreOptionsBtn')
+      .click();
+
+    cy.get('@clickCleanTableBtn')
+      .click();
+
+    cy.get(`.${prefix}--modal-container[aria-label="Clean table data"]`)
+      .find('button')
+      .contains('Clean table data')
+      .click();
+
+    // Check values in 'SMP success on parent (%)' and 'Non-orchard pollen contam. (%)' columns of the table
+    cy.get('#0-volume-value-input')
+      .should('have.value', '');
+
+    cy.get('#1-volume-value-input')
+      .should('have.value', '');
+
+    // Check upload button functionality
+    cy.get('button.upload-button')
+      .click({force: true});
+
+    cy.get(`.${prefix}--modal-container[aria-label="Seedlot registration"]`)
+      .should('be.visible');
+
+    cy.get('button')
+      .contains('Cancel')
+      .click();
+
+    cy.get(`.${prefix}--modal-container[aria-label="Seedlot registration"]`)
+      .should('not.be.visible');
+  });
 });
