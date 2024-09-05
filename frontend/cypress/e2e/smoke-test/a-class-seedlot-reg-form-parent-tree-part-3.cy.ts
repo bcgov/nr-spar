09bbcf07569c2ea43ccf199ed31e7f1c8bc4ca6b
Nisarg <nisargvv@gmail.com>
2024-09-04 18:40:04 -0700

    feat: create new parent tree cypress test part-3

---
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts
@@ -0,0 +1,34 @@
+import prefix from '../../../src/styles/classPrefix';
+
+describe('A Class Seedlot Registration form, Parent Tree and SMP part-3(Calculation of SMP mix)', () => {
+  let regFormData: {
+    parentTree: {
+      calculationTitle: string;
+      calculationSubtitle: string;
+    }
+  };
+
+  let seedlotNum: string;
+  const speciesKey = 'pli';
+
+  beforeEach(() => {
+    // Login
+    cy.login();
+    cy.fixture('aclass-reg-form').then((fData) => {
+      regFormData = fData;
+    });
+
+    cy.fixture('aclass-seedlot').then((fData) => {
+      cy.task('getData', fData[speciesKey].species).then((sNumber) => {
+        seedlotNum = sNumber as string;
+        const url = `/seedlots/a-class-registration/${seedlotNum}/?step=5`;
+        cy.visit(url);
+        cy.url().should('contains', url);
+        cy.get('#parent-tree-step-tab-list-id')
+          .find('button')
+          .contains('SMP success on parent')
+          .click();
+      });
+    });
+  });
+});
