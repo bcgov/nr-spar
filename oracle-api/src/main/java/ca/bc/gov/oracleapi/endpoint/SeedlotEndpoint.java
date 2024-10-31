833dea28e66f93034f46fafb0aac82307b602950
Xiao Peng <xiaopeng@Xiaos-MacBook-Pro.local>
2024-10-30 20:07:53 -0700

    feat: backend api

---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/endpoint/SeedlotEndpoint.java
@@ -0,0 +1,61 @@
+package ca.bc.gov.oracleapi.endpoint;
+
+import ca.bc.gov.oracleapi.config.SparLog;
+import ca.bc.gov.oracleapi.entity.Seedlot;
+import ca.bc.gov.oracleapi.repository.SeedlotRepository;
+import ca.bc.gov.oracleapi.security.RoleAccessConfig;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** This class exposes seedlot resources API. */
+@RestController
+@RequestMapping("/api/seedlot")
+@Tag(name = "seedlot", description = "Resource to retrieve seedlot")
+public class SeedlotEndpoint {
+
+  private SeedlotRepository seedlotRepository;
+
+  SeedlotEndpoint(SeedlotRepository seedlotRepository) {
+    this.seedlotRepository = seedlotRepository;
+  }
+
+  /**
+   * Retrieve a seedlot by seedlot number.
+   *
+   * @return A record of {@link Seedlot}.
+   */
+  @GetMapping(produces = "application/json")
+  @Operation(
+      summary = "Retrieve a seedlot by seedlot number",
+      description = "Retrieve a seedlot by seedlot number ")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Returns a record of seedlot",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Seedlot.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Access token is missing or invalid",
+            content = @Content(schema = @Schema(implementation = Void.class)))
+      })
+  @RoleAccessConfig({"SPAR_TSC_ADMIN", "SPAR_MINISTRY_ORCHARD", "SPAR_NONMINISTRY_ORCHARD"})
+  public Seedlot getSeedlotBySeedlotNumber(Long seedlotNumber) {
+    SparLog.info("Fetch a seedlot by seedlot number");
+
+    Seedlot seedlot = seedlotRepository.findSeedlotBySeedlotNumber(seedlotNumber);
+    SparLog.info("{} valid seedlot found.", seedlot);
+
+    return seedlot;
+  }
+}
---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/entity/Seedlot.java
@@ -0,0 +1,29 @@
+package ca.bc.gov.oracleapi.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/** This class presents a funding source to an agency seedlot owner. */
+@Getter
+@Setter
+@Entity
+@Table(name = "SEEDLOT")
+@Schema(description = "Represents a partial seedlot object in the database")
+public class Seedlot {
+
+  @Id
+  @Column(name = "SEEDLOT_NUMBER")
+  @Schema(description = "The number of a seedlot", example = "16258")
+  private String seedlotNumber;
+
+  @Column(name = "ORIGINAL_SEED_QTY")
+  @Schema(
+      description = "The original quantity of seeds in the seedlot",
+      example = "1")
+  private String originalSeedQty;
+}
