edec21d8bad3e1456360c3c485c43f384aab027b
Nisarg <nisargvv@gmail.com>
2024-11-01 13:20:38 -0700

    feat: add parent tree calculations test

---
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-calculations-part-1.cy.ts
@@ -0,0 +1,23 @@
+import prefix from '../../../src/styles/classPrefix';
+
+describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', () => {
+  let seedlotNum: string;
+  const speciesKey = 'pli';
+
+  beforeEach(() => {
+    // Login
+    cy.login();
+
+    cy.fixture('aclass-seedlot').then((fData) => {
+      cy.task('getData', fData[speciesKey].species).then((sNumber) => {
+        seedlotNum = sNumber as string;
+        const url = `/seedlots/a-class-registration/${seedlotNum}/?step=5`;
+        cy.visit(url);
+        cy.url().should('contains', url);
+      });
+    });
+  });
+
+  it('Check Parent tree contribution summary', () => {
+  });
+});
