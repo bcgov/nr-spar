5a073a99c13a860173a9c6f9405014d968266c47
Nisarg <nisargvv@gmail.com>
2024-11-01 13:21:04 -0700

    feat: add cypress test name

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -31,7 +31,8 @@ export default defineConfig({
       '**/a-class-seedlot-reg-form-extraction.cy.ts',
       '**/a-class-seedlot-reg-form-parent-tree-part-1.cy.ts',
       '**/a-class-seedlot-reg-form-parent-tree-part-2.cy.ts',
-      '**/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts'
+      '**/a-class-seedlot-reg-form-parent-tree-part-3.cy.ts',
+      '**/a-class-seedlot-reg-form-parent-tree-calculations-part-1.cy.ts'
     ],
     chromeWebSecurity: false,
     retries: {
