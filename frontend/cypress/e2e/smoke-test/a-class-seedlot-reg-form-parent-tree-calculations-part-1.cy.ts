73b37fec198a8994aac56275adc914b016038d53
Nisarg <nisargvv@gmail.com>
2024-11-04 13:47:32 -0800

    feat: add orchard section

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-calculations-part-1.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-calculations-part-1.cy.ts
@@ -2,7 +2,7 @@ import prefix from '../../../src/styles/classPrefix';
 
 describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', () => {
   let seedlotNum: string;
-  const speciesKey = 'pli';
+  const speciesKey = 'fdi';
 
   beforeEach(() => {
     // Login
@@ -16,6 +16,45 @@ describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', (
         cy.url().should('contains', url);
       });
     });
+  });
+
+  it('Orchard selection', () => {
+    const url = `/seedlots/a-class-registration/${seedlotNum}/?step=4`;
+    cy.visit(url);
+
+    cy.get('#primary-orchard-selection')
+      .siblings(`button.${prefix}--list-box__menu-icon[title="Open"]`)
+      .click();
+
+    // Select primary orchard
+    cy.get(`.${prefix}--list-box--expanded`)
+      .find('ul li')
+      .as('orchardDropdown')
+      .contains('324 - BAILEY - S - PRD')
+      .click();
+
+    // Select female gametic contribution methodology
+    cy.get('#orchard-female-gametic')
+      .siblings()
+      .click();
+
+    cy.get(`.${prefix}--list-box--expanded`)
+      .find('ul li')
+      .contains('F1 - Visual Estimate')
+      .click();
+
+    // Select male gametic contribution methodology
+    cy.get('#orchard-male-gametic')
+      .siblings()
+      .click();
+
+    cy.get(`.${prefix}--list-box--expanded`)
+      .find('ul li')
+      .contains('M2 - Pollen Volume Estimate by Partial Survey')
+      .click();
+
+    // Save changes
+    cy.saveSeedlotRegFormProgress();
   });
 
   it('Check Parent tree contribution summary', () => {
