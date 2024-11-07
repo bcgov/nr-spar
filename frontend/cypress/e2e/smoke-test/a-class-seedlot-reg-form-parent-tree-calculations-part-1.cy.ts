93d04212abb3b2820d569b04be1e95ce6bf243bb
Nisarg <nisargvv@gmail.com>
2024-11-06 19:07:30 -0800

    feat: fix count issues

--- frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-calculations-part-1.cy.ts
+++ frontend/cypress/e2e/smoke-test/a-class-seedlot-reg-form-parent-tree-calculations-part-1.cy.ts
@@ -3,12 +3,12 @@ import prefix from '../../../src/styles/classPrefix';
 describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', () => {
   let seedlotNum: string;
   const speciesKey = 'fdi';
-  let totalParentTrees: number;
-  let totalConeCount: number;
-  let totalPollenCount: number;
-  let effectivePopulationSize: number;
-  let firstConeValue: number;
-  let firstPollenValue: number;
+  let totalParentTrees: number = 0;
+  let totalConeCount: number = 0;
+  let totalPollenCount: number = 0;
+  let effectivePopulationSize: number = 0;
+  let firstConeValue: number = 0;
+  let firstPollenValue: number = 0;
 
   beforeEach(() => {
     // Login
@@ -69,14 +69,14 @@ describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', (
 
     // Upload csv file
     cy.get('button.upload-button')
-      .click({force: true});
+      .click({ force: true });
 
     cy.get(`.${prefix}--modal-container[aria-label="Seedlot registration"]`)
       .should('be.visible');
 
     cy.get(`.${prefix}--file`)
       .find(`input.${prefix}--file-input`)
-      .selectFile('cypress/fixtures/Seedlot_composition_template_FDI.csv', {force: true});
+      .selectFile('cypress/fixtures/Seedlot_composition_template_FDI.csv', { force: true });
 
     cy.get('button')
       .contains('Import file and continue')
@@ -94,38 +94,62 @@ describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', (
       .find('tr')
       .then((row) => {
         totalParentTrees = row.length;
+        cy.log('Total rows:', totalParentTrees);
+        cy.get('#totalnumber\\ of\\ parent\\ trees')
+          .should('have.value', totalParentTrees);
+
+        // Get total cone counts
+        for (let i = 0; i < totalParentTrees; i += 1) {
+          cy.get('.parent-tree-step-table-container-col')
+            .find('table tbody tr')
+            .eq(i)
+            .find('td:nth-child(2) input')
+            .invoke('val')
+            .then(($value: any) => {
+              cy.log("Cone count:", $value);
+              totalConeCount = totalConeCount + Number($value);
+
+              if (i === (totalParentTrees - 1)) {
+                cy.get('#totalnumber\\ of\\ cone\\ count')
+                  .should('have.value', totalConeCount);
+              }
+            });
+        }
+
+        // Get total pollen counts
+        for (let i = 0; i < totalParentTrees; i += 1) {
+          cy.get('.parent-tree-step-table-container-col')
+            .find('table tbody tr')
+            .eq(i)
+            .find('td:nth-child(3) input')
+            .invoke('val')
+            .then(($value) => {
+              cy.log("Pollen count:", $value);
+              totalPollenCount = totalPollenCount + Number($value);
+
+              if (i === (totalParentTrees - 1)) {
+                cy.get('#totalnumber\\ of\\ pollen\\ count')
+                  .should('have.value', totalPollenCount);
+              }
+            });
+        }
       });
 
-    cy.get('input[id$="-coneCount-value-input"]') // Select all inputs in coneCount column
-      .then($inputs => {
-        // Extract values, convert them to numbers, and calculate the sum using Lodash
-        const sum = Cypress._.sum(
-          $inputs.toArray().map(input => Number(input.innerText))
-        );
-    
-        totalConeCount = sum;
-        cy.log('Sum of coneCount column:', sum);
+    // Click 'Calculate metrics' button
+    cy.get('.gen-worth-cal-row')
+      .find('button')
+      .contains('Calculate metrics')
+      .click();
+
+    cy.wait(3000);
+
+    // Store Ne value to a variable
+    cy.get('#effectivepopulation\\ size\\ \\(ne\\)')
+      .invoke('val')
+      .then(($input: any) => {
+        effectivePopulationSize = $input;
+        cy.log("effective population size:", effectivePopulationSize);
       });
-
-    cy.get('input[id$="-pollenCount-value-input"]') // Select all inputs in pollenCount column
-      .then($inputs => {
-        // Extract values, convert them to numbers, and calculate the sum using Lodash
-        const sum = Cypress._.sum(
-          $inputs.toArray().map(input => Number(input.innerText))
-        );
-    
-        totalPollenCount = sum;
-        cy.log('Sum of pollenCount column:', sum);
-      });
-
-    cy.get('#totalnumber of parent trees')
-      .should('have.value', totalParentTrees);
-
-    cy.get('#totalnumber of cone count')
-      .should('have.value', totalConeCount);
-
-    cy.get('#totalnumber of pollen count')
-      .should('have.value', totalPollenCount);
 
     // Save changes
     cy.saveSeedlotRegFormProgress();
@@ -135,50 +159,45 @@ describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', (
     // Wait for the table to load
     cy.get('#parentTreeNumber', { timeout: 10000 });
 
-    // Click 'Calculate metrics' button
-    cy.get('.gen-worth-cal-row')
-      .find('button')
-      .contains('Calculate metrics')
-      .click();
-
-    // Store Ne value to a variable
-    cy.get('#effectivepopulation size (ne)')
-      .invoke('val')
-      .then(($input: any) => {
-        effectivePopulationSize = $input;
-      });
-
-    // Store first cone count to a variable
     cy.get('#8021-coneCount-value-input')
       .invoke('val')
       .then(($input: any) => {
+        // Store first cone count to a variable
         firstConeValue = $input;
+
+        // Clear cone count of first row
+        cy.get('#8021-coneCount-value-input')
+          .clear()
+          .type('0')
+          .blur();
+
+        cy.log("total cone count:", totalConeCount);
+        cy.log("first cone count:", firstConeValue);
+        // Check new total cone count
+        cy.get('#totalnumber\\ of\\ cone\\ count')
+          .should('have.value', (totalConeCount - firstConeValue));
       });
 
-    // Store first pollen count to a variable
     cy.get('#8021-pollenCount-value-input')
       .invoke('val')
       .then(($input: any) => {
+        // Store first pollen count to a variable
         firstPollenValue = $input;
+
+        // Clear pollen count of first row
+        cy.get('#8021-pollenCount-value-input')
+          .clear()
+          .type('0')
+          .blur();
+
+        // Check new total parent trees
+        cy.get('#totalnumber\\ of\\ parent\\ trees')
+          .should('have.value', (totalParentTrees - 1));
+
+        // Check new total pollen count
+        cy.get('#totalnumber\\ of\\ pollen\\ count')
+          .should('have.value', (totalPollenCount - firstPollenValue));
       });
-
-    // Clear cone count and pollen count of first row
-    cy.get('#8021-coneCount-value-input')
-      .clear()
-      .type('0');
-
-    cy.get('#8021-pollenCount-value-input')
-      .clear()
-      .type('0');
-
-    cy.get('#totalnumber of parent trees')
-      .should('have.value', (totalParentTrees - 1));
-
-    cy.get('#totalnumber of cone count')
-      .should('have.value', (totalConeCount - firstConeValue));
-
-    cy.get('#totalnumber of pollen count')
-      .should('have.value', (totalPollenCount - firstPollenValue));
 
     // Click 'Calculate metrics' button again
     cy.get('.gen-worth-cal-row')
@@ -186,8 +205,15 @@ describe('A Class Seedlot Registration form, Parent Tree Calculations Part 1', (
       .contains('Calculate metrics')
       .click();
 
+    cy.wait(3000);
+
     // Check Ne value after clearing first parent tree row
-    cy.get('#effectivepopulation size (ne)')
-      .should('be.lessThan', effectivePopulationSize);
+    cy.get('#effectivepopulation\\ size\\ \\(ne\\)')
+      .invoke('val')
+      .then($value => {
+        cy.log("Value:",$value);
+        cy.log("Effective Population Size:",effectivePopulationSize);
+        expect(Number($value)).to.be.lessThan(Number(effectivePopulationSize));
+      });
   });
 });
