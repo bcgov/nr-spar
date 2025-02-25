0333b0782201b4cbf1db1728b810b38b6fe98e95
Nisarg <nisargvv@gmail.com>
2025-02-24 16:32:46 -0800

    feat: add heading

---
+++ frontend/src/views/CONSEP/TestingActivities/MoistureContent/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FlexGrid, Row } from '@carbon/react';
+import ROUTES from '../../../../routes/constants';
+import Breadcrumbs from '../../../../components/Breadcrumbs';
+import PageTitle from '../../../../components/PageTitle';
+
+const MoistureContent = () => {
+  const createBreadcrumbItems = () => {
+    const crumbsList = [];
+    crumbsList.push({ name: 'CONSEP', path: ROUTES.CONSEP_FAVOURITE_ACTIVITIES });
+    crumbsList.push({ name: 'Testing activities search', path: ROUTES.TESTING_REQUESTS_REPORT });
+    crumbsList.push({ name: 'Testing list', path: ROUTES.TESTING_ACTIVITIES_LIST });
+    return crumbsList;
+  };
+
+  return (
+    <FlexGrid className="consep-moisture-content">
+      <Row className="seedlot-details-breadcrumb">
+        <Breadcrumbs crumbs={createBreadcrumbItems()} />
+      </Row>
+      <Row className="page-title">
+        <PageTitle
+          title="Moisture content oven for seedlot 64132"
+          enableFavourite
+        />
+      </Row>
+    </FlexGrid>
+  );
+};
+
+export default MoistureContent;
