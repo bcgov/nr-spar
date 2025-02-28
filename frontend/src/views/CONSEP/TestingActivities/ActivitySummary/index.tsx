ef489acfbe91619cdd1e9789b2492e3414d66cef
Nisarg <nisargvv@gmail.com>
2025-02-27 17:47:35 -0800

    feat: add accordian section

--- frontend/src/views/CONSEP/TestingActivities/ActivitySummary/index.tsx
+++ frontend/src/views/CONSEP/TestingActivities/ActivitySummary/index.tsx
@@ -1,46 +1,49 @@
 import React from 'react';
-import { FlexGrid, Row, Column } from '@carbon/react';
+import {
+  FlexGrid,
+  Row,
+  Column,
+  Accordion,
+  AccordionItem
+} from '@carbon/react';
 
 import './styles.scss';
 
 const ActivitySummary = () => (
   <FlexGrid className="activity-summary-container">
-    <Row className="activity-summary">
-      <Column sm={4} md={8} lg={16} className="activity-summary-title-section">
-        <p className="activity-summary-title">
-          Activity summary
-        </p>
-      </Column>
-      <Column sm={4} md={8} lg={16} className="activity-summary-info-section">
-        <Row>
-          <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-            <p className="activity-summary-info-label">
-              Activity
-            </p>
-          </Column>
-          <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-            <p className="activity-summary-info-label">
-              Seedlot number
-            </p>
-          </Column>
-          <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-            <p className="activity-summary-info-label">
-              Request ID
-            </p>
-          </Column>
-          <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-            <p className="activity-summary-info-label">
-              Species
-            </p>
-          </Column>
-          <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-            <p className="activity-summary-info-label">
-              Test result
-            </p>
-          </Column>
-        </Row>
-      </Column>
-    </Row>
+    <Accordion className="activity-summary">
+      <AccordionItem open title="Activity summary">
+        <Column sm={4} md={8} lg={16} className="activity-summary-info-section">
+          <Row>
+            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
+              <p className="activity-summary-info-label">
+                Activity
+              </p>
+            </Column>
+            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
+              <p className="activity-summary-info-label">
+                Seedlot number
+              </p>
+            </Column>
+            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
+              <p className="activity-summary-info-label">
+                Request ID
+              </p>
+            </Column>
+            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
+              <p className="activity-summary-info-label">
+                Species
+              </p>
+            </Column>
+            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
+              <p className="activity-summary-info-label">
+                Test result
+              </p>
+            </Column>
+          </Row>
+        </Column>
+      </AccordionItem>
+    </Accordion>
   </FlexGrid>
 );
 
