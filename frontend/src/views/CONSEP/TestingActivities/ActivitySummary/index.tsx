69377c27021a830062e331c2ad16704564343c68
Nisarg <nisargvv@gmail.com>
2025-03-03 17:14:08 -0800

    fix: remove column grids and add icon

--- frontend/src/views/CONSEP/TestingActivities/ActivitySummary/index.tsx
+++ frontend/src/views/CONSEP/TestingActivities/ActivitySummary/index.tsx
@@ -8,40 +8,43 @@ import {
 } from '@carbon/react';
 
 import './styles.scss';
+import { Report } from '@carbon/icons-react';
 
 const ActivitySummary = () => (
   <FlexGrid className="activity-summary-container">
     <Accordion className="activity-summary">
-      <AccordionItem open title="Activity summary">
-        <Column sm={4} md={8} lg={16} className="activity-summary-info-section">
-          <Row>
-            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-              <p className="activity-summary-info-label">
-                Activity
-              </p>
-            </Column>
-            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-              <p className="activity-summary-info-label">
-                Seedlot number
-              </p>
-            </Column>
-            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-              <p className="activity-summary-info-label">
-                Request ID
-              </p>
-            </Column>
-            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-              <p className="activity-summary-info-label">
-                Species
-              </p>
-            </Column>
-            <Column sm={2} md={2} lg={3} xlg={2} className="info-col">
-              <p className="activity-summary-info-label">
-                Test result
-              </p>
-            </Column>
-          </Row>
-        </Column>
+      <AccordionItem
+        open
+        title="Activity summary"
+        renderIcon={() => <Report />}
+      >
+        <Row>
+          <Column className="info-col">
+            <p className="activity-summary-info-label">
+              Activity
+            </p>
+          </Column>
+          <Column className="info-col">
+            <p className="activity-summary-info-label">
+              Seedlot number
+            </p>
+          </Column>
+          <Column className="info-col">
+            <p className="activity-summary-info-label">
+              Request ID
+            </p>
+          </Column>
+          <Column className="info-col">
+            <p className="activity-summary-info-label">
+              Species
+            </p>
+          </Column>
+          <Column className="info-col">
+            <p className="activity-summary-info-label">
+              Test result
+            </p>
+          </Column>
+        </Row>
       </AccordionItem>
     </Accordion>
   </FlexGrid>
