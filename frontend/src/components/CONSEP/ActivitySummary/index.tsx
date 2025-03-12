71ab4cc5eaebebb94dee071d07afae548d7add0e
Nisarg Vadgama <nisargvv@gmail.com>
2025-03-11 20:14:34 -0700

    feat: add activity summary value section

--- frontend/src/components/CONSEP/ActivitySummary/index.tsx
+++ frontend/src/components/CONSEP/ActivitySummary/index.tsx
@@ -6,9 +6,17 @@ import {
   AccordionItem
 } from '@carbon/react';
 
+import renderFieldValue from './FieldValue';
+import { ActivitySummaryType } from '../../../types/ActivitySummaryType';
+
 import './styles.scss';
 
-const ActivitySummary = () => (
+interface ActivitySummaryProps {
+  seedlot?: ActivitySummaryType;
+  isFetching: boolean;
+}
+
+const ActivitySummary = ({ seedlot, isFetching }: ActivitySummaryProps) => (
   <div className="activity-summary-container">
     <Accordion className="activity-summary">
       <AccordionItem
@@ -20,26 +28,41 @@ const ActivitySummary = () => (
             <p className="activity-summary-info-label">
               Activity
             </p>
+            {
+              renderFieldValue('activity', isFetching, seedlot)
+            }
           </Column>
           <Column className="info-col">
             <p className="activity-summary-info-label">
               Seedlot number
             </p>
+            {
+              renderFieldValue('seedlotNumber', isFetching, seedlot)
+            }
           </Column>
           <Column className="info-col">
             <p className="activity-summary-info-label">
               Request ID
             </p>
+            {
+              renderFieldValue('requestId', isFetching, seedlot)
+            }
           </Column>
           <Column className="info-col">
             <p className="activity-summary-info-label">
               Species
             </p>
+            {
+              renderFieldValue('species', isFetching, seedlot)
+            }
           </Column>
           <Column className="info-col">
             <p className="activity-summary-info-label">
               Test result
             </p>
+            {
+              renderFieldValue('testResult', isFetching, seedlot)
+            }
           </Column>
         </Row>
       </AccordionItem>
