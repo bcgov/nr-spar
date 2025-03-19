1ccdbbbf21b3936f1f511ca323b2704a13f507b0
Nisarg Vadgama <nisargvv@gmail.com>
2025-03-19 16:45:00 -0700

    feat: create a model class for numbers

---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/MoistureContentAverageDto.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Numbers {
+    private List<Double> numbers;
+
+    public Numbers(List<Double> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Double> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<Double> numbers) {
+        this.numbers = numbers;
+    }
+}
