2e18db139d9c82aec7ac9ae96a3c30951d1f2b05
Nisarg Vadgama <nisargvv@gmail.com>
2025-03-19 17:01:54 -0700

    feat: add class to handle HTTP request

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/MoistureContentAverageDto.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/MoistureContentAverageDto.java
@@ -1,4 +1,10 @@
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 public class Numbers {
     private List<Double> numbers;
@@ -24,4 +30,24 @@ public class AverageService {
       }
       return numbers.stream().mapToDouble(Double::doubleValue).average().orElse(0);
   }
+}
+
+@RestController
+public class AverageController {
+    private final AverageService averageService;
+
+    @Autowired
+    public AverageController(AverageService averageService) {
+        this.averageService = averageService;
+    }
+
+    @PostMapping("/calculate-average")
+    public ResponseEntity<Double> calculateAverage(@RequestBody Numbers numbers) {
+        try {
+            double average = averageService.calculateAverage(numbers.getNumbers());
+            return ResponseEntity.status(HttpStatus.OK).body(average);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
