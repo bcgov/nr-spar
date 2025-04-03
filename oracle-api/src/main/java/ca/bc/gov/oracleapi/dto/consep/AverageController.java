89be301992d54d23d6065a803e57f404aaf47ac5
Nisarg Vadgama <nisargvv@gmail.com>
2025-04-03 15:43:36 -0700

    feat: move classes to their own files

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/AverageController.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/AverageController.java
@@ -1,36 +1,11 @@
-import java.util.List;
+package ca.bc.gov.oracleapi.dto.consep;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-public class Numbers {
-    private List<Double> numbers;
-
-    public Numbers(List<Double> numbers) {
-        this.numbers = numbers;
-    }
-
-    public List<Double> getNumbers() {
-        return numbers;
-    }
-
-    public void setNumbers(List<Double> numbers) {
-        this.numbers = numbers;
-    }
-}
-
-/** This class calculateas the average. */
-public class AverageService {
-  public double calculateAverage(List<Double> numbers) {
-      if (numbers.isEmpty()) {
-          throw new RuntimeException("Cannot calculate average of empty list");
-      }
-      return numbers.stream().mapToDouble(Double::doubleValue).average().orElse(0);
-  }
-}
 
 @RestController
 public class AverageController {
---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/Numbers.java
@@ -0,0 +1,15 @@
+package ca.bc.gov.oracleapi.dto.consep;
+
+import java.util.List;
+
+public class Numbers {
+  private List<Double> numbers;
+
+  public List<Double> getNumbers() {
+      return numbers;
+  }
+
+  public void setNumbers(List<Double> numbers) {
+      this.numbers = numbers;
+  }
+}
