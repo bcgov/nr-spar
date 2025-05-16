944e692c7339d79523a23835b848bb7b1b144f73
mgaseta <matheus.gaseta@encora.com>
2025-05-16 09:57:52 -0300

    test: fixing test declaration

--- oracle-api/src/test/java/ca/bc/gov/oracleapi/service/consep/ActivityServiceTest.java
+++ oracle-api/src/test/java/ca/bc/gov/oracleapi/service/consep/ActivityServiceTest.java
@@ -46,8 +46,8 @@ class ActivityServiceTest {
 
     activityEntity = new ActivityEntity();
     activityEntity.setTestCategoryCode("TEST");
-    activityEntity.setActualBeginDateTime(LocalDateTime.now().plusDays(1));
-    activityEntity.setActualEndDateTime(LocalDateTime.now().plusDays(2));
+    activityEntity.setActualBeginDateTime(LocalDateTime.now().minusDays(1));
+    activityEntity.setActualEndDateTime(LocalDateTime.now().minusDays(2));
     activityEntity.setRiaComment("Test comment");
   }
 
