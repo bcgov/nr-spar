cc6fb45baea37067a3d9fdc9fae924a5b2ea24e6
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2025-07-10 16:36:32 -0700

    feat: add date test

--- frontend/cypress/e2e/smoke-test-05/36-moisture-content.cy.ts
+++ frontend/cypress/e2e/smoke-test-05/36-moisture-content.cy.ts
@@ -5,8 +5,38 @@ describe('Moisture Content Screen page', () => {
     cy.url().should('contains', '/consep/manual-moisture-content/:riaKey');
   });
   it('should load and display manual moisture content page correctly', () => {
+    // Check if the page title is displayed correctly
     cy.get('.consep-moisture-content-title')
       .find('h1')
       .contains('Moisture content cones for seedlot');
+
+    // Check if the table title is displayed correctly
+    cy.get('.activity-result-actions-title')
+      .find('h3')
+      .contains('Activity results per replicate');
+  });
+
+  it('Check Date inputs', () => {
+    // Check if the date input has a placeholder
+    cy.get('.bx--date-picker-container')
+      .find('input')
+      .should('have.attr', 'placeholder', 'yyyy/mm/dd');
+
+    cy.get('#moisture-content-end-date-picker')
+      .clear()
+      .type('2025-05-28')
+      .blur();
+
+    // Invalid start date
+    cy.get('#moisture-content-start-date-picker')
+      .clear()
+      .type('2025-05-29')
+      .blur();
+
+    // Valid start date
+    cy.get('#moisture-content-start-date-picker')
+      .clear()
+      .type('2025-05-27')
+      .blur();
   });
 });
