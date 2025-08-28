adc4fbec5660b86628656c3c822c86e56429c276
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2025-08-28 13:33:24 -0700

    feat: add sorting for dates

--- frontend/src/components/SeedlotTable/utils.ts
+++ frontend/src/components/SeedlotTable/utils.ts
@@ -1,11 +1,22 @@
 import { SeedlotDisplayType } from '../../types/SeedlotType';
 
+const dateSort = (x: SeedlotDisplayType['createdAt'], y: SeedlotDisplayType['createdAt']) => {
+  const dateX = new Date(x).getTime();
+  const dateY = new Date(y).getTime();
+  return dateX - dateY;
+};
+
 export const sortByKey = (
   dataToSort: SeedlotDisplayType[],
   headerId: keyof SeedlotDisplayType | null,
   direction: string
 ) => {
-  if (headerId && direction === 'ASC') {
+  if (headerId && ['createdAt', 'lastUpdatedAt'].includes(headerId)) {
+    return dataToSort.sort((a, b) => {
+      const result = dateSort(a[headerId], b[headerId]);
+      return direction === 'ASC' ? result : -result;
+    });
+  } if (headerId && direction === 'ASC') {
     return dataToSort.sort((a, b) => {
       if (a[headerId] > b[headerId]) {
         return 1;
@@ -15,8 +26,7 @@ export const sortByKey = (
       }
       return 0;
     });
-  }
-  if (headerId && direction === 'DESC') {
+  } if (headerId && direction === 'DESC') {
     return dataToSort.sort((a, b) => {
       if (a[headerId] < b[headerId]) {
         return 1;
