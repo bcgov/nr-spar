a5965e25384f18fb035809367a69130c734e8c96
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2025-09-04 15:55:10 -0700

    fix: update agency input error code

--- frontend/cypress/e2e/smoke-test-03/21-create-a-class-seedlot-pli.cy.ts
+++ frontend/cypress/e2e/smoke-test-03/21-create-a-class-seedlot-pli.cy.ts
@@ -57,15 +57,21 @@ describe('Create PLI Seedlot', () => {
     cy.contains('.section-title', 'Seedlot information')
       .click();
 
-    cy.get('.loading-input-wrapper').eq(1).then(($section) => {
-      if ($section.find('#agency-number-input-error-msg').length) {
-        // Error message is present, re-enter the agency number
-        cy.get('#agency-number-input')
-          .clear()
-          .type('11', { delay: TYPE_DELAY })
-          .blur({ force: true });
-      }
-    });
+    cy.contains('.bx--label', 'Agency location code')
+      .closest('.loading-input-wrapper')
+      .then(($section) => {
+        if ($section.find('#agency-number-input-error-msg').length) {
+          // Error message is present, re-enter the agency number
+          cy.get('#agency-number-input')
+            .clear()
+            .type('11', { delay: TYPE_DELAY })
+            .blur({ force: true });
+
+          // Click on the section title
+          cy.contains('.section-title', 'Seedlot information')
+            .click();
+        }
+      });
 
     // Click on button Create seedlot number
     cy.get('.submit-button')
