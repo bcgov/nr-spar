b9cdfc737b50d38b1bd96ed5397b87c2b0590adb
MCatherine <Mcatherine1994@gmail.com>
2025-12-30 10:16:31 -0800

    fix(frontend): #2225 Update default cypress timeout to 1m (#2277)

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'cypress';
 import cypressSplit from 'cypress-split';
-import { FIFTEEN_SECONDS } from './cypress/constants';
+import { ONE_MINUTE } from './cypress/constants';
 
 declare const require: any;
 
@@ -43,7 +43,7 @@ export default defineConfig({
     retries: {
       runMode: 0
     },
-    defaultCommandTimeout: FIFTEEN_SECONDS,
+    defaultCommandTimeout: ONE_MINUTE,
     video: true,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
