28d4965f0b0590bd67e2e750614657f1205da286
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-01-28 10:53:55 -0800

    fix: remove unwanted API data

--- frontend/cypress/support/mockApiConsep.ts
+++ frontend/cypress/support/mockApiConsep.ts
@@ -8,11 +8,6 @@ export function mockMoistureContentApi() {
   ).as('GET_moisture_content_cone');
 
   cy.intercept(
-    { method: 'GET', url: '**/api/seedlots/60662' },
-    { statusCode: 200, fixture: 'default-seedlot-detail.json' }
-  ).as('GET_seedlot_detail');
-
-  cy.intercept(
     { method: 'GET', url: '**/api/forest-clients/00012797' },
     { statusCode: 200, fixture: 'forest-client.json' }
   ).as('GET_forest_client');
