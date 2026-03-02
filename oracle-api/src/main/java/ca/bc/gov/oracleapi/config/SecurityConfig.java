4456e311b52a63ae5eef7a19ef34e1678612884e
mcatherine1994 <mcatherine1994@gmail.com>
2026-03-02 10:55:02 -0800

    fix: #2374 try to upgrade java dependecy package versions

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/config/SecurityConfig.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/config/SecurityConfig.java
@@ -49,13 +49,6 @@ public class SecurityConfig {
                 customize
                     .requestMatchers("/api/**")
                     .authenticated()
-                    // Explicitly permit OpenAPI and Swagger UI endpoints
-                    .requestMatchers(
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                    )
-                    .permitAll()
                     .requestMatchers(HttpMethod.OPTIONS, "/**")
                     .permitAll()
                     .anyRequest()
