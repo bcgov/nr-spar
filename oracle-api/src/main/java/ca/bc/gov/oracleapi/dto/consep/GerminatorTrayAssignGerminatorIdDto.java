d44895976280c17038ed149f81de835dfa2330f2
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-02 15:19:59 -0800

    feat: remove germinatorTrayId as it will be fetched from URL

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/GerminatorTrayAssignGerminatorIdDto.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/dto/consep/GerminatorTrayAssignGerminatorIdDto.java
@@ -2,7 +2,6 @@ package ca.bc.gov.oracleapi.dto.consep;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 /**
@@ -10,10 +9,6 @@ import jakarta.validation.constraints.Size;
  * assigning a germinator ID to an existing {@link ca.bc.gov.oracleapi.entity.consep.GerminatorTrayEntity}.
  */
 public record GerminatorTrayAssignGerminatorIdDto(
-    @Schema(description = "Primary key of the germinator tray", example = "101")
-    @NotNull
-    Integer germinatorTrayId,
-
     @Schema(description = "Germinator ID to assign to the tray", example = "1")
     @Size(max = 1)
     @NotBlank
