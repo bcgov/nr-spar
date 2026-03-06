561d9beb38906953ccee7efaa8063e5389a7d3e4
mcatherine1994 <mcatherine1994@gmail.com>
2026-03-05 16:49:28 -0800

    fix: #2357 remove unused import in test

--- oracle-api/src/test/java/ca/bc/gov/oracleapi/service/consep/GerminatorTrayServiceTest.java
+++ oracle-api/src/test/java/ca/bc/gov/oracleapi/service/consep/GerminatorTrayServiceTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
