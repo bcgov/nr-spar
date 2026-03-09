af7b0ab1ec2fccebd467e62ca4540bb20bdfd638
mcatherine1994 <mcatherine1994@gmail.com>
2026-03-09 14:17:02 -0700

    fix: #2249 update sonar cloud warning

--- oracle-api/src/test/java/ca/bc/gov/oracleapi/service/consep/ActivitySearchServiceTest.java
+++ oracle-api/src/test/java/ca/bc/gov/oracleapi/service/consep/ActivitySearchServiceTest.java
@@ -248,8 +248,8 @@ class ActivitySearchServiceTest {
     assertThat(activitySearchResponseDto.seedlotSample()).isEqualTo(seedlotSample);
     assertThat(activitySearchResponseDto.riaSkey()).isEqualTo(riaSkey);
     assertThat(activitySearchResponseDto.activityTypeCd()).isEqualTo(testTypes.get(0));
-    assertThat(activitySearchResponseDto.germTestInd()).isEqualTo(0);
-    assertThat(activitySearchResponseDto.assignedTrayInd()).isEqualTo(0);
+    assertThat(activitySearchResponseDto.germTestInd()).isZero();
+    assertThat(activitySearchResponseDto.assignedTrayInd()).isZero();
 
     verify(activitySearchRepository, times(1)).searchTestingActivities(
         lotNumbers, testTypes, activityIds, germinatorTrayId,
