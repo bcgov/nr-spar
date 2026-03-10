25861de246dbbdee22be73a149de44da2f9c8e94
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-10 12:47:30 -0700

    feat: create an entity for germinator tray content

---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/entity/consep/GerminationTrayContentsEntity.java
@@ -0,0 +1,71 @@
+package ca.bc.gov.oracleapi.entity.consep;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "CNS58TRAYCONTENTS", schema = "CONSEP")
+@Schema(
+    description = "Represents a germinator tray contents in the CONSEP database."
+)
+public class GerminationTrayContentsEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "GERMINATOR_TRAY_ID", precision = 5, scale = 0, nullable = false)
+  private Integer germinatorTrayId;
+  @Column(name = "VEGETATION_ST", length = 8)
+  private String vegetationSt;
+  @Column(name = "ACTIVITY_TYPE_CD", length = 3, nullable = false)
+  private String activityTypeCd;
+  @Column(name = "ACTUAL_START_DATE")
+  private LocalDateTime actualStartDate;
+  @Column(name = "DATE_CREATED", nullable = false)
+  private LocalDateTime dateCreated;
+  @Column(name = "RIA_SKEY", precision = 10, scale = 0, nullable = false)
+  private Integer riaSkey;
+  @Column(name = "REQUEST_ID", scale = 0)
+  private Integer requestId;
+  @Column(name = "REQUEST_SKEY", precision = 10, scale = 0)
+  private Integer requestSkey;
+  @Column(name = "ITEM_ID", length = 1)
+  private String itemId;
+  @Column(name = "REQUEST_TYPE_ST", length = 3)
+  private String requestTypeSt;
+  @Column(name = "SEEDLOT_NUMBER", length = 13)
+  private String seedlotNumber;
+  @Column(name = "SOAK_START_DATE")
+  private LocalDateTime soakStartDate;
+  @Column(name = "SOAK_END_DATE")
+  private LocalDateTime soakEndDate;
+  @Column(name = "SEED_WITHDRAWAL_DATE")
+  private LocalDateTime seedWithdrawalDate;
+  @Column(name = "WARM_STRAT_START_DATE")
+  private LocalDateTime warmStratStartDate;
+  @Column(name = "DRYBACK_START_DATE")
+  private LocalDateTime drybackStartDate;
+  @Column(name = "GERMINATOR_ENTRY")
+  private LocalDateTime germinatorEntry;
+  @Column(name = "STRAT_START_DT")
+  private LocalDateTime stratStartDt;
+  @Column(name = "GERMINATOR_ID", length = 1)
+  private String germinatorId;
+  @Column(name = "STNDRD_ACTIVITY_ID", length = 3)
+  private String stndrdActivityId;
+  @Column(name = "TEST_CATEGORY_CD", length = 3)
+  private String testCategoryCd;
+  @Column(name = "TEST_COMPLETE_IND", precision = 5, scale = 0)
+  private Integer testCompleteInd;
+  @Column(name = "ACCEPT_RESULT_IND", precision = 5, scale = 0)
+  private Integer acceptResultInd;
+}
