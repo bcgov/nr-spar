f6f3ee2d22450b78eea9850a96ac9be6983ae704
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-11 16:35:33 -0700

    feat: add delete and revision count query

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminatorTrayRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminatorTrayRepository.java
@@ -2,9 +2,35 @@ package ca.bc.gov.oracleapi.repository.consep;
 
 import ca.bc.gov.oracleapi.entity.consep.GerminatorTrayEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * This interface enables the germinator tray entity from consep to be retrieved from the database.
  */
 public interface GerminatorTrayRepository extends JpaRepository<GerminatorTrayEntity, Integer> {
+  @Modifying
+  @Transactional
+  @Query(
+      """
+      DELETE FROM GerminatorTrayEntity gtr
+       WHERE gtr.germinatorTrayId = :trayId
+         AND gtr.revisionCount = :revisionCount
+      """)
+  int deleteByIdAndRevisionCount(
+      @Param("trayId") Integer trayId, @Param("revisionCount") Long revisionCount);
+
+  @Modifying
+  @Transactional
+  @Query(
+      """
+      UPDATE GerminatorTrayEntity gtr
+         SET gtr.revisionCount = gtr.revisionCount + 1
+       WHERE gtr.germinatorTrayId = :trayId
+         AND gtr.revisionCount = :revisionCount
+      """)
+  int incrementRevisionCountWithVersionCheck(
+      @Param("trayId") Integer trayId, @Param("revisionCount") Long revisionCount);
 }
