047f322d26553e702f0dec8d574927f9518d453d
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-10 17:08:59 -0700

    feat: create a new repository for germ tray contents

---
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminationTrayContentsRepository.java
@@ -0,0 +1,24 @@
+package ca.bc.gov.oracleapi.repository.consep;
+
+import ca.bc.gov.oracleapi.entity.consep.GerminationTrayContentsEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Repository for germination tray contents view.
+ */
+public interface GerminationTrayContentsRepository
+    extends JpaRepository<GerminationTrayContentsEntity, Integer> {
+
+  @Query("""
+      SELECT gtc
+      FROM GerminationTrayContentsEntity gtc
+      WHERE gtc.germinatorTrayId = :trayId
+      ORDER BY gtc.seedlotNumber, gtc.requestId
+      """)
+  List<GerminationTrayContentsEntity> findByGerminatorTrayId(
+    @Param("trayId") Integer trayId
+  );
+}
