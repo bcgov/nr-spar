ab91343c92a3a7d9efabe2f4eb68a375b3f267a3
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-12 15:05:55 -0700

    fix: schema output

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/endpoint/consep/GerminatorTrayEndpoint.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/endpoint/consep/GerminatorTrayEndpoint.java
@@ -9,6 +9,7 @@ import ca.bc.gov.oracleapi.response.ValidationExceptionResponse;
 import ca.bc.gov.oracleapi.security.RoleAccessConfig;
 import ca.bc.gov.oracleapi.service.consep.GerminatorTrayService;
 import ca.bc.gov.oracleapi.service.consep.TestResultService;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -57,7 +58,7 @@ public class GerminatorTrayEndpoint {
   @ApiResponse(
       responseCode = "201",
       description = "Successfully assigned trays for the provided activities.",
-      content = @Content(schema = @Schema(implementation = GerminatorTrayCreateResponseDto.class))
+      content = @Content(array = @ArraySchema(schema = @Schema(implementation = GerminatorTrayContentsDto.class)))
   )
   @ApiAuthResponse
   @RoleAccessConfig({ "SPAR_TSC_SUBMITTER", "SPAR_TSC_SUPERVISOR" })
@@ -133,7 +134,7 @@ public class GerminatorTrayEndpoint {
   @ApiResponse(
       responseCode = "200",
       description = "Successfully retrieved tests for the germinator tray.",
-      content = @Content(schema = @Schema(implementation = GerminatorTrayContentsDto.class)))
+      content = @Content(array = @ArraySchema(schema = @Schema(implementation = GerminatorTrayContentsDto.class))))
   @ApiAuthResponse
   @RoleAccessConfig({"SPAR_TSC_SUBMITTER", "SPAR_TSC_SUPERVISOR"})
   public List<GerminatorTrayContentsDto> getTestsByTrayId(@PathVariable @Positive Integer germinatorTrayId) {
