fa27effd9fc6c9cca3678207a120917466831815
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-18 14:16:11 -0700

    feat: update the query to add updateTimestamp field

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminationTrayContentsRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminationTrayContentsRepository.java
@@ -1,8 +1,8 @@
 package ca.bc.gov.oracleapi.repository.consep;
 
 import ca.bc.gov.oracleapi.entity.consep.GerminationTrayContentsEntity;
-import java.util.List;
 import java.math.BigDecimal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,12 +14,11 @@ public interface GerminationTrayContentsRepository
     extends JpaRepository<GerminationTrayContentsEntity, BigDecimal> {
 
   @Query("""
-      SELECT gtc
+      SELECT gtc, a.updateTimestamp
       FROM GerminationTrayContentsEntity gtc
+      LEFT JOIN ActivityEntity a ON a.riaKey = gtc.riaSkey
       WHERE gtc.germinatorTrayId = :trayId
       ORDER BY gtc.seedlotNumber, gtc.requestId
       """)
-  List<GerminationTrayContentsEntity> findByGerminatorTrayId(
-    @Param("trayId") Integer trayId
-  );
+  List<Object[]> findByGerminatorTrayId(@Param("trayId") Integer trayId);
 }
