00a2d83b13f5b54a370727dfdd7b963445b1ab90
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-03-25 17:00:06 -0700

    feat: add query for search tray

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminatorTrayRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/GerminatorTrayRepository.java
@@ -1,6 +1,9 @@
 package ca.bc.gov.oracleapi.repository.consep;
 
+import ca.bc.gov.oracleapi.dto.consep.GerminatorTraySearchResponseDto;
 import ca.bc.gov.oracleapi.entity.consep.GerminatorTrayEntity;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -25,4 +28,37 @@ public interface GerminatorTrayRepository extends JpaRepository<GerminatorTrayEn
        WHERE t.germinatorTrayId = :germinatorTrayId
       """)
   int deleteByGerminatorTrayId(@Param("germinatorTrayId") Integer germinatorTrayId);
+
+  @Query("""
+      SELECT DISTINCT new ca.bc.gov.oracleapi.dto.consep.GerminatorTraySearchResponseDto(
+          tray.germinatorTrayId,
+          tray.activityTypeCd,
+          tray.actualStartDate,
+          tray.dateCreated,
+          tray.revisionCount,
+          tray.systemTrayNo,
+          tray.germinatorId
+      )
+      FROM GerminatorTrayEntity tray
+      WHERE EXISTS (
+          SELECT tr.riaKey
+          FROM TestResultEntity tr
+          JOIN ActivityEntity a
+            ON a.riaKey = tr.riaKey
+          WHERE tr.germinatorTrayId = tray.germinatorTrayId
+            AND (
+              :seedlotOrFamilyLot IS NULL
+              OR a.seedlotNumber = :seedlotOrFamilyLot
+              OR a.familyLotNumber = :seedlotOrFamilyLot
+            )
+            AND (:requestId IS NULL OR a.requestId = :requestId)
+            AND (:itemId IS NULL OR a.itemId = :itemId)
+      )
+      ORDER BY tray.systemTrayNo, tray.germinatorTrayId
+      """)
+  List<GerminatorTraySearchResponseDto> searchGerminatorTrays(
+      @Param("seedlotOrFamilyLot") String seedlotOrFamilyLot,
+      @Param("requestId") String requestId,
+      @Param("itemId") String itemId
+  );
 }
