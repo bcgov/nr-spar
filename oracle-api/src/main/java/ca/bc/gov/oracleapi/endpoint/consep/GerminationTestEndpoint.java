01e2c5ff116df8d9439af4401fa1763b48e2a414
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-05-20 12:04:02 -0700

    feat: add exception handler negative value of daily germ skey

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/endpoint/consep/GerminationTestEndpoint.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/endpoint/consep/GerminationTestEndpoint.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +55,12 @@ public class GerminationTestEndpoint {
   public DailyAbnormalResponseDto getDailyAbnormalCounts(
       @PathVariable @Positive BigDecimal dailyGermSkey) {
     return testResultService.getDailyAbnormalCounts(dailyGermSkey);
+  }
+
+  @ExceptionHandler(jakarta.validation.ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public java.util.Map<String, String> handleConstraintViolation(
+      jakarta.validation.ConstraintViolationException ex) {
+    return java.util.Map.of("message", "dailyGermSkey must be greater than 0");
   }
 }
