25fa75f475afc70e80f24e39dbd14318e2defe63
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-05-20 12:04:53 -0700

    fix; remove the redundant query

--- oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/DailyAbnormalRepository.java
+++ oracle-api/src/main/java/ca/bc/gov/oracleapi/repository/consep/DailyAbnormalRepository.java
@@ -3,8 +3,6 @@ package ca.bc.gov.oracleapi.repository.consep;
 import ca.bc.gov.oracleapi.entity.consep.DailyAbnormalEntity;
 import java.math.BigDecimal;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface DailyAbnormalRepository extends JpaRepository<DailyAbnormalEntity, BigDecimal> {
   /**
@@ -13,9 +11,5 @@ public interface DailyAbnormalRepository extends JpaRepository<DailyAbnormalEnti
    * @param dailyGermSkey the daily germ surrogate key identifier
    * @return the DailyAbnormalEntity matching the given dailyGermSkey, or null if not found
    */
-  @Query("""
-      SELECT d FROM DailyAbnormalEntity d
-      WHERE d.dailyGermSkey = :dailyGermSkey
-      """)
-  DailyAbnormalEntity findByDailyGermSkey(@Param("dailyGermSkey") BigDecimal dailyGermSkey);
+  DailyAbnormalEntity findByDailyGermSkey(BigDecimal dailyGermSkey);
 }
