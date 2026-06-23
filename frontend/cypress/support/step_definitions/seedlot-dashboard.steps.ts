de3f4d364d8d6a38c198994a0db6446cf0af95c9
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-06-23 13:13:03 -0700

    fix: update the file using new fixture file

--- frontend/cypress/support/step_definitions/seedlot-dashboard.steps.ts
+++ frontend/cypress/support/step_definitions/seedlot-dashboard.steps.ts
@@ -1,47 +1,44 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-import { NavigationLabels } from '../../utils/labels';
-import { SeedlotDashboardDataType, SeedlotRegFixtureType } from '../../definitions';
-
-let seedlotDashboardData: SeedlotDashboardDataType;
-
-Given('the seedlot dashboard fixture is loaded', () => {
-  cy.fixture('seedlot-dashboard').then((fData) => {
-    seedlotDashboardData = fData;
-  });
-});
+import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { SeedlotRegFixtureType } from '../../definitions';
 
 When('I visit the seedlot dashboard page', () => {
   cy.visit('/seedlots');
 });
 
-Then('the seedlot dashboard page should display the correct title and subtitle', () => {
-  cy.isPageTitle(NavigationLabels.Seedlots);
-
-  cy.get('.recent-seedlots-title-section')
-    .find('h2')
-    .should('have.text', seedlotDashboardData.secondSectionTitle);
-
-  cy.get('.recent-seedlots-title-section')
-    .find('.recent-seedlots-subtitle')
-    .should('have.text', seedlotDashboardData.secondSectionSubtitle);
+Then('I can see the title {string}', (title: string) => {
+  cy.isPageTitle(title);
 });
 
-let seedlotTableData: SeedlotRegFixtureType = {};
-let speciesKeys: string[] = [];
+Then('I can see the section title {string}', (title: string) => {
+  cy.get('.recent-seedlots-title-section')
+    .find('h2')
+    .should('have.text', title);
+});
 
-Given('the a-class seedlot fixture is loaded', () => {
-  cy.fixture('aclass-seedlot').then((jsonData: SeedlotRegFixtureType) => {
-    seedlotTableData = jsonData;
-    speciesKeys = Object.keys(jsonData);
-  });
+Then('I can see the section subtitle {string}', (subtitle: string) => {
+  cy.get('.recent-seedlots-title-section')
+    .find('.recent-seedlots-subtitle')
+    .should('have.text', subtitle);
+});
+
+When('I view the seedlot species table', () => {
+  cy.get('.recent-seedlots-table').should('be.visible');
 });
 
 Then('all seedlot species should exist in the seedlot table', () => {
-  Cypress._.times(speciesKeys.length - 1, (i) => {
-    const { species } = seedlotTableData[speciesKeys[i]];
-    cy.task('getData', species).then((sNumber) => {
-      const seedlotNumber = sNumber as string;
-      cy.get(`#seedlot-table-cell-${seedlotNumber}-seedlotSpecies`).should('have.text', species);
+  cy.get('@aClassSeedlotData').then((fixtureData: any) => {
+    const tableData: SeedlotRegFixtureType = fixtureData;
+    const keys = Object.keys(tableData);
+
+    Cypress._.times(keys.length - 1, (i) => {
+      const { species } = tableData[keys[i]];
+
+      cy.task('getData', species).then((sNumber) => {
+        const seedlotNumber = sNumber as string;
+
+        cy.get(`#seedlot-table-cell-${seedlotNumber}-seedlotSpecies`)
+          .should('have.text', species);
+      });
     });
   });
 });
