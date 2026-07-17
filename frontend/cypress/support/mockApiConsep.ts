c2b491f46e3115932a5f9559b1399a5b82e0dee7
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-07-16 20:08:11 -0700

    feat: add delete api for replicate

--- frontend/cypress/support/mockApiConsep.ts
+++ frontend/cypress/support/mockApiConsep.ts
@@ -22,9 +22,41 @@ export function mockMoistureContentApi() {
     { method: 'DELETE', url: '**/api/moisture-content-cone/514330/*' },
     { statusCode: 200, fixture: 'moisture-seedlot-replicate-info.json' }
   ).as('DELETE_replicate');
+}
 
+export function mockPurityContentApi() {
   cy.intercept(
     { method: 'GET', url: '**/api/purity-tests/79082/*' },
     { statusCode: 200, fixture: 'purity-seedlot-replicate-info.json' }
   ).as('GET_purity_content_cone');
+
+  cy.intercept(
+    { method: 'DELETE', url: '**/api/purity-tests/debris/79082/**' },
+    {
+      statusCode: 200,
+      body: [
+        {
+          riaKey: 79082,
+          replicateNumber: 1,
+          debrisSeqNumber: 5889566,
+          debrisRank: 1,
+          debrisTypeCode: 'BRS'
+        },
+        {
+          riaKey: 79082,
+          replicateNumber: 1,
+          debrisSeqNumber: 5889571,
+          debrisRank: 2,
+          debrisTypeCode: 'CHF'
+        },
+        {
+          riaKey: 79082,
+          replicateNumber: 1,
+          debrisSeqNumber: null,
+          debrisRank: 3,
+          debrisTypeCode: 'OTH'
+        }
+      ]
+    }
+  ).as('DELETE_impurity');
 }
