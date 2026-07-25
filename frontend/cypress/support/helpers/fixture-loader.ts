26e11a45a73b3f447fb0e2b46de2f04f0d07baa6
Nisarg Vadgama <nisarg.vadgama@bc.gov.ca>
2026-07-24 20:03:56 -0700

    feat: add a reusable fixture loader code

---
+++ frontend/cypress/support/helpers/fixture-loader.ts
@@ -0,0 +1,9 @@
+export const loadFixtureAndAlias = <T>(
+  fixtureName: string,
+  alias: string,
+  assign?: (data: T) => void
+) => cy.fixture(fixtureName).then((data: T) => {
+  if (assign) assign(data);
+  cy.wrap(data).as(alias);
+  return data;
+});
