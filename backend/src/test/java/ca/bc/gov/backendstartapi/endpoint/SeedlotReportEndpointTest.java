dee90f43756e5fc6c8682be2cd434b9a5c37ffc8
mightycox <j.acox@hotmail.com>
2026-08-18 12:30:58 -0700

    feat: 5321 - sonar fixes

--- backend/src/test/java/ca/bc/gov/backendstartapi/endpoint/SeedlotReportEndpointTest.java
+++ backend/src/test/java/ca/bc/gov/backendstartapi/endpoint/SeedlotReportEndpointTest.java
@@ -13,10 +13,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(SeedlotReportEndpoint.class)
